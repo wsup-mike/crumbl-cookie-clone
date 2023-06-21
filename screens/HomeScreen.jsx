1ca50044ce2523af8ec2db20332fe67341679127
Mike Ramos <michaelangeloram8@gmail.com>
2023-06-21 16:37:51 -0500

    next commit

--- screens/HomeScreen.jsx
+++ screens/HomeScreen.jsx
@@ -1,11 +1,13 @@
-import { View, Text } from 'react-native'
+import { View, Text, ScrollView, SafeAreaView } from 'react-native'
 import React from 'react'
 
 const HomeScreen = () => {
   return (
-    <View classname='flex'>
-      <Text>The Mutha Frikin Crumbl Cookie Clone App</Text>
-    </View>
+    <SafeAreaView className='flex-1 items-center justify-center'>
+        <View>
+            <Text className='text-3xl text-center'>HOME SCREEN: The Mutha Frikin Crumbl Cookie Clone App</Text>
+        </View>
+    </SafeAreaView>
   )
 }
 
