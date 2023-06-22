7f20227af6b26317f43a03a4aa4304ff6c1e3fcd
Mike Ramos <michaelangeloram8@gmail.com>
2023-06-21 21:37:57 -0500

    import and add all screens for each tab

--- RootNavigator.js
+++ RootNavigator.js
@@ -4,6 +4,10 @@ import React from "react";
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import HomeScreen from "./screens/HomeScreen";
+import OrderScreen from "./screens/OrderScreen";
+import WalletScreen from "./screens/WalletScreen";
+import GiftCardScreen from "./screens/GiftCardScreen";
+import AccountScreen from "./screens/AccountScreen";
 
 const Stack = createNativeStackNavigator();
 const Tab = createBottomTabNavigator();
@@ -12,6 +16,10 @@ const RootNavigator = () => {
   return (
     <Tab.Navigator>
       <Tab.Screen name="Menu" component={HomeScreen} />
+      <Tab.Screen name="Order" component={OrderScreen} />
+      <Tab.Screen name="Wallet" component={WalletScreen} />
+      <Tab.Screen name="Gifts" component={GiftCardScreen} />
+      <Tab.Screen name="More" component={AccountScreen} />
     </Tab.Navigator>
   );
 };
