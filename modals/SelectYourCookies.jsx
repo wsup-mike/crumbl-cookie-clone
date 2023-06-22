bb3628dce78fa313fc2fdf75105a198154b25b31
Mike Ramos <michaelangeloram8@gmail.com>
2023-06-22 10:14:21 -0500

    Cr8 SelectYourCookies modal

---
+++ modals/SelectYourCookies.jsx
@@ -0,0 +1,14 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+const SelectYourCookies = () => {
+  return (
+    <SafeAreaView className='flex-1 items-center justify-center'>
+        <View>
+            <Text className='text-3xl text-center'>SelectYourCookies modal: To choose cookies</Text>
+        </View>
+    </SafeAreaView>
+  )
+}
+
+export default SelectYourCookies
