9fe5930d74251a3de86f3e730f058cf5caa889be
Mike Ramos <michaelangeloram8@gmail.com>
2023-06-22 08:56:57 -0500

    Cr8 new stack

---
+++ stacks/RegularCookieSizeStack.js
@@ -0,0 +1,12 @@
+import { View, Text } from "react-native";
+import React from "react";
+
+const RegularCookieSizeStack = () => {
+  return (
+    <View>
+      <Text>RegularCookieSizeStack</Text>
+    </View>
+  );
+};
+
+export default RegularCookieSizeStack;
