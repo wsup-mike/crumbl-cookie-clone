2369ca0ee5d938959ac8ced9a810ccf39291094e
Mike Ramos <michaelangeloram8@gmail.com>
2023-06-22 08:58:43 -0500

    Cr8 new stack

---
+++ stacks/WeeklySubscriptionDeliveryStack.js
@@ -0,0 +1,12 @@
+import { View, Text } from "react-native";
+import React from "react";
+
+const WeeklySubscriptionDeliveryStack = () => {
+  return (
+    <View>
+      <Text>WeeklySubscriptionDeliveryStack</Text>
+    </View>
+  );
+};
+
+export default WeeklySubscriptionDeliveryStack;
