42fede71279ae05c469924abd758c5210681aa26
Mike Ramos <michaelangeloram8@gmail.com>
2023-06-21 22:04:52 -0500

    Remove redundancies

--- App.js
+++ App.js
@@ -1,11 +1,8 @@
 import { StatusBar } from "expo-status-bar";
 import { NavigationContainer } from "@react-navigation/native";
-import { createNativeStackNavigator } from "@react-navigation/native-stack";
-import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
-import HomeScreen from "./screens/HomeScreen";
 import RootNavigator from "./RootNavigator";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 
-const Stack = createNativeStackNavigator();
 const Tab = createBottomTabNavigator();
 
 export default function App() {
