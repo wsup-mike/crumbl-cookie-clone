6ac7dbbe4d2e5448cd47914ddc1763562f4bf783
Mike Ramos <michaelangeloram8@gmail.com>
2023-06-22 10:36:11 -0500

    Cr8 TipPaymentBakers modal

---
+++ modals/TipPaymentBakers.jsx
@@ -0,0 +1,14 @@
+import { View, Text, SafeAreaView } from 'react-native'
+import React from 'react'
+
+const TipPaymentBakers = () => {
+  return (
+    <SafeAreaView className='flex-1 items-center justify-center'>
+        <View>
+            <Text className='text-3xl text-center'>TipPaymentBakers modal</Text>
+        </View>
+    </SafeAreaView>
+  )
+}
+
+export default TipPaymentBakers
