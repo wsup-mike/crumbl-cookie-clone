dc88747186ba4d7e8e60dfd79b3319e180dd8988
Mike Ramos <michaelangeloram8@gmail.com>
2023-06-22 10:47:40 -0500

    Cr8 UseGiftCardVoucher modal

---
+++ modals/UseGiftCardVoucher.jsx
@@ -0,0 +1,14 @@
+import { View, Text, SafeAreaView } from 'react-native'
+import React from 'react'
+
+const UseGiftCardVoucher = () => {
+  return (
+    <SafeAreaView className='flex-1 items-center justify-center'>
+        <View>
+            <Text className='text-3xl text-center'>UseGiftCardVoucher modal: Basic instruction modal</Text>
+        </View>
+    </SafeAreaView>
+  )
+}
+
+export default UseGiftCardVoucher
