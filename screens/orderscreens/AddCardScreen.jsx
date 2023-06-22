e65fc0dc4b32c3dd4773599c65dedcbedfcc8eb5
Mike Ramos <michaelangeloram8@gmail.com>
2023-06-22 10:41:55 -0500

    Cr8 AddCardScreen screen

---
+++ screens/orderscreens/AddCardScreen.jsx
@@ -0,0 +1,14 @@
+import { View, Text, SafeAreaView } from 'react-native'
+import React from 'react'
+
+const AddCardScreen = () => {
+  return (
+    <SafeAreaView className='flex-1 items-center justify-center'>
+        <View>
+            <Text className='text-3xl text-center'>AddCardScreen: To add credit card</Text>
+        </View>
+    </SafeAreaView>
+  )
+}
+
+export default AddCardScreen
