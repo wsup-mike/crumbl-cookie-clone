45f031c610358979f90e45868b2f2204eebf4111
Mike Ramos <michaelangeloram8@gmail.com>
2023-06-22 09:42:50 -0500

    Cr8 SelectPickupLocation modal

---
+++ modals/SelectPickupLocation.jsx
@@ -0,0 +1,14 @@
+import { View, Text, SafeAreaView } from 'react-native'
+import React from 'react'
+
+const SelectPickupLocation = () => {
+  return (
+    <SafeAreaView className='flex-1 items-center justify-center'>
+        <View>
+            <Text className='text-3xl text-center'>Select Pickup Location Modal</Text>
+        </View>
+    </SafeAreaView>
+  )
+}
+
+export default SelectPickupLocation
