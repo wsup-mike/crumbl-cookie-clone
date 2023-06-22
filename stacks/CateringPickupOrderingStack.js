5c71b5c581133f7f267a203bc578775579bd39d6
Mike Ramos <michaelangeloram8@gmail.com>
2023-06-22 08:55:23 -0500

    Cr8 new stack

---
+++ stacks/CateringPickupOrderingStack.js
@@ -0,0 +1,12 @@
+import { View, Text } from "react-native";
+import React from "react";
+
+const CateringPickupOrderingStack = () => {
+  return (
+    <View>
+      <Text>CateringPickupOrderingStack</Text>
+    </View>
+  );
+};
+
+export default CateringPickupOrderingStack;
