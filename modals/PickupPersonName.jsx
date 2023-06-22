f4e8aa9bf530c44a527a1fc8f0c5ca17d096baa2
Mike Ramos <michaelangeloram8@gmail.com>
2023-06-22 10:24:36 -0500

    Cr8 PickpersonName modal

---
+++ modals/PickupPersonName.jsx
@@ -0,0 +1,14 @@
+import { View, Text, SafeAreaView } from 'react-native'
+import React from 'react'
+
+const PickupPersonName = () => {
+  return (
+    <SafeAreaView className='flex-1 items-center justify-center'>
+        <View>
+            <Text className='text-3xl text-center'>PickupPersonName modal</Text>
+        </View>
+    </SafeAreaView>
+  )
+}
+
+export default PickupPersonName
