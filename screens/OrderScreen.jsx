69976b1bc77f02b5f2835c5789b4c5a33ef802a2
Mike Ramos <michaelangeloram8@gmail.com>
2023-06-22 17:12:02 -0500

    Add 5 order buttons

--- screens/OrderScreen.jsx
+++ screens/OrderScreen.jsx
@@ -1,11 +1,37 @@
-import { View, Text, SafeAreaView} from 'react-native'
+import { View, Text, SafeAreaView, TouchableOpacity } from 'react-native'
 import React from 'react'
+
 
 const OrderScreen = () => {
   return (
     <SafeAreaView className='flex-1 items-center justify-center'>
         <View>
             <Text className='text-3xl text-center'>ORDER SCREEN:5 Options to Order</Text>
+            <TouchableOpacity>
+              <View className='items-center py-4'>
+                <Text>StorePickupOrderingStack</Text>
+              </View>
+            </TouchableOpacity>
+            <TouchableOpacity>
+              <View className='items-center py-4'>
+                <Text>DeliveryOrderingStack</Text>
+              </View>
+            </TouchableOpacity>
+            <TouchableOpacity>
+              <View className='items-center py-4'>
+                <Text>CateringPickupOrderingStack</Text>
+              </View>
+            </TouchableOpacity>
+            <TouchableOpacity>
+              <View className='items-center py-4'>
+                <Text>ShippingOrderingStack</Text>
+              </View>
+            </TouchableOpacity>
+            <TouchableOpacity>
+              <View className='items-center py-4'>
+                <Text>SubscriptionOrderingStack</Text>
+              </View>
+            </TouchableOpacity>
         </View>
     </SafeAreaView>
   )
