254876336765afc036bd80fbb8b90cdd29b5842a
Mike Ramos <michaelangeloram8@gmail.com>
2023-06-22 08:56:07 -0500

    Cr8 new stack

---
+++ stacks/MiniCookieSizeStack.js
@@ -0,0 +1,12 @@
+import { View, Text } from "react-native";
+import React from "react";
+
+const MiniCookieSizeStack = () => {
+  return (
+    <View>
+      <Text>MiniCookieSizeStack</Text>
+    </View>
+  );
+};
+
+export default MiniCookieSizeStack;
