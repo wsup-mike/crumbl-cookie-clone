2a389839472bbd2ba0b07f2969fb200e87a64be9
Mike Ramos <michaelangeloram8@gmail.com>
2023-06-22 08:57:09 -0500

    Cr8 new stack

---
+++ stacks/ShippingOrderingStack.js
@@ -0,0 +1,12 @@
+import { View, Text } from "react-native";
+import React from "react";
+
+const ShippingOrderingStack = () => {
+  return (
+    <View>
+      <Text>ShippingOrderingStack</Text>
+    </View>
+  );
+};
+
+export default ShippingOrderingStack;
