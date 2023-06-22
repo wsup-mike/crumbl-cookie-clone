2504b0218086c5f11517461b7fe6f756a48197dd
Mike Ramos <michaelangeloram8@gmail.com>
2023-06-22 08:55:57 -0500

    Cr8 new stack

---
+++ stacks/DeliveryOrderingStack.js
@@ -0,0 +1,12 @@
+import { View, Text } from "react-native";
+import React from "react";
+
+const DeliveryOrderingStack = () => {
+  return (
+    <View>
+      <Text>DeliveryOrderingStack</Text>
+    </View>
+  );
+};
+
+export default DeliveryOrderingStack;
