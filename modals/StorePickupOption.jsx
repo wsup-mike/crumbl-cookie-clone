74207f628e23ea821ea698e0f2bd903ccaa7a8ca
Mike Ramos <michaelangeloram8@gmail.com>
2023-06-22 09:47:46 -0500

    Cr8 StorePickupOption modal

---
+++ modals/StorePickupOption.jsx
@@ -0,0 +1,14 @@
+import { View, Text, SafeAreaView } from 'react-native'
+import React from 'react'
+
+const StorePickupOption = () => {
+  return (
+    <SafeAreaView className='flex-1 items-center justify-center'>
+        <View>
+            <Text className='text-3xl text-center'>Store Pickup Option Modal: Carryout or Curbside</Text>
+        </View>
+    </SafeAreaView>
+  )
+}
+
+export default StorePickupOption
