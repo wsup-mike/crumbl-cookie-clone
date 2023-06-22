74877f30653d68d47ab30e39134d41a4eeaa5063
Mike Ramos <michaelangeloram8@gmail.com>
2023-06-22 10:50:41 -0500

    Cr8 OrderIsProcessingScreen screen

---
+++ screens/orderscreens/OrderIsProcessingScreen.jsx
@@ -0,0 +1,14 @@
+import { View, Text, SafeAreaView } from 'react-native'
+import React from 'react'
+
+const OrderIsProcessingScreen = () => {
+  return (
+    <SafeAreaView className='flex-1 items-center justify-center'>
+        <View>
+            <Text className='text-3xl text-center'>OrderIsProcessingScreen screen - Animated</Text>
+        </View>
+    </SafeAreaView>
+  )
+}
+
+export default OrderIsProcessingScreen
