11f4e38d833028f86cfe3e340bd57699a5c1d4ff
Mike Ramos <michaelangeloram8@gmail.com>
2023-06-22 10:08:10 -0500

    Edit CarryoutOrderScreen - format

--- screens/orderscreens/CarryoutOrderScreen.jsx
+++ screens/orderscreens/CarryoutOrderScreen.jsx
@@ -1,11 +1,13 @@
-import { View, Text } from 'react-native'
+import { View, Text, SafeAreaView } from 'react-native'
 import React from 'react'
 
 const CarryoutOrderScreen = () => {
   return (
-    <View>
-      <Text>CarryoutOrderScreen</Text>
-    </View>
+    <SafeAreaView className='flex-1 items-center justify-center'>
+        <View>
+            <Text className='text-3xl text-center'>Store Pickup / Carry Out Order Screen</Text>
+        </View>
+    </SafeAreaView>
   )
 }
 
