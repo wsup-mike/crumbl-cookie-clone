2650a5f002c0e67b09b8c417dae3a292aad2e672
Mike Ramos <michaelangeloram8@gmail.com>
2023-06-22 09:15:44 -0500

    Wire up OrderStack

--- RootNavigator.js
+++ RootNavigator.js
@@ -7,6 +7,7 @@ import OrderScreen from "./screens/OrderScreen";
 import WalletScreen from "./screens/WalletScreen";
 import GiftCardScreen from "./screens/GiftCardScreen";
 import AccountScreen from "./screens/AccountScreen";
+import OrderStack from "./stacks/OrderStack";
 
 const Tab = createBottomTabNavigator();
 
@@ -14,7 +15,7 @@ const RootNavigator = () => {
   return (
     <Tab.Navigator>
       <Tab.Screen name="Menu" component={HomeScreen} />
-      <Tab.Screen name="Order" component={OrderScreen} />
+      <Tab.Screen name="Order" component={OrderStack} />
       <Tab.Screen name="Wallet" component={WalletScreen} />
       <Tab.Screen name="Gifts" component={GiftCardScreen} />
       <Tab.Screen name="More" component={AccountScreen} />
