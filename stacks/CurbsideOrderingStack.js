4f8e41e2b9a2d63bba85512e181fddc4de5f43d1
Mike Ramos <michaelangeloram8@gmail.com>
2023-06-22 08:55:37 -0500

    Cr8 new stack

---
+++ stacks/CurbsideOrderingStack.js
@@ -0,0 +1,12 @@
+import { View, Text } from "react-native";
+import React from "react";
+
+const CurbsideOrderingStack = () => {
+  return (
+    <View>
+      <Text>CurbsideOrderingStack</Text>
+    </View>
+  );
+};
+
+export default CurbsideOrderingStack;
