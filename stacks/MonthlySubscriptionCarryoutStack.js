207d5cd26da7583c957eaf948d2dbd84fc5903b5
Mike Ramos <michaelangeloram8@gmail.com>
2023-06-22 08:56:21 -0500

    Cr8 new stack

---
+++ stacks/MonthlySubscriptionCarryoutStack.js
@@ -0,0 +1,12 @@
+import { View, Text } from "react-native";
+import React from "react";
+
+const MonthlySubscriptionCarryoutStack = () => {
+  return (
+    <View>
+      <Text>MonthlySubscriptionCarryoutStack</Text>
+    </View>
+  );
+};
+
+export default MonthlySubscriptionCarryoutStack;
