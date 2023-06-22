fa5bae935380263972de274381ca03469cfe80dc
Mike Ramos <michaelangeloram8@gmail.com>
2023-06-22 09:54:08 -0500

    Implement 4 stack screens

--- stacks/StorePickupOrderingStack.js
+++ stacks/StorePickupOrderingStack.js
@@ -3,11 +3,29 @@ import React from "react";
 import { createStackNavigator } from "@react-navigation/stack";
 import SelectPickupLocation from "../modals/SelectPickupLocation";
 import StorePickupOption from "./modals/StorePickupOption";
+import CarryoutOrderingStack from "/stacks/CarryoutOrderingStack";
+import CurbsideOrderingStack from "./stacks/CurbsideOrderingStack";
 
 const Stack = createStackNavigator();
 
 const StorePickupOrderingStack = () => {
-  return <Stack.Navigator></Stack.Navigator>;
+  return (
+    <Stack.Navigator>
+      <Stack.Screen
+        name="SelectPickupLocation"
+        component={SelectPickupLocation}
+      />
+      <Stack.Screen name="StorePickupOption" component={StorePickupOption} />
+      <Stack.Screen
+        name="CarryoutOrderingStack"
+        component={CarryoutOrderingStack}
+      />
+      <Stack.Screen
+        name="CurbsideOrderingStack"
+        component={CurbsideOrderingStack}
+      />
+    </Stack.Navigator>
+  );
 };
 
 export default StorePickupOrderingStack;
