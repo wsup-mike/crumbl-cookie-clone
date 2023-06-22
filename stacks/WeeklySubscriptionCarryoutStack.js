4608a7dfa75f800ba7c0130f648df96e9f517fbc
Mike Ramos <michaelangeloram8@gmail.com>
2023-06-22 08:58:33 -0500

    Cr8 new stack

---
+++ stacks/WeeklySubscriptionCarryoutStack.js
@@ -0,0 +1,12 @@
+import { View, Text } from "react-native";
+import React from "react";
+
+const WeeklySubscriptionCarryoutStack = () => {
+  return (
+    <View>
+      <Text>WeeklySubscriptionCarryoutStack</Text>
+    </View>
+  );
+};
+
+export default WeeklySubscriptionCarryoutStack;
