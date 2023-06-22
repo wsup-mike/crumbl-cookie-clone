ea8cea18780814679838e7d7fedde20a7814142a
Mike Ramos <michaelangeloram8@gmail.com>
2023-06-22 10:19:34 -0500

    Cr8 CookiesGame modal

---
+++ modals/CookiesGame.jsx
@@ -0,0 +1,14 @@
+import { View, Text, SafeAreaView } from 'react-native'
+import React from 'react'
+
+const CookiesGame = () => {
+  return (
+    <SafeAreaView className='flex-1 items-center justify-center'>
+        <View>
+            <Text className='text-3xl text-center'>CookiesGame modal: merch item</Text>
+        </View>
+    </SafeAreaView>
+  )
+}
+
+export default CookiesGame
