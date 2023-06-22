216d7a3d2d0b9baa16f970a0dcc57a8f1648b3d3
Mike Ramos <michaelangeloram8@gmail.com>
2023-06-22 11:00:46 -0500

    Cr8 ReasonForCancellation modal

---
+++ modals/ReasonForCancellation.jsx
@@ -0,0 +1,14 @@
+import { View, Text, SafeAreaView } from 'react-native'
+import React from 'react'
+
+const ReasonForCancellation = () => {
+  return (
+    <SafeAreaView className='flex-1 items-center justify-center'>
+        <View>
+            <Text className='text-3xl text-center'>ReasonForCancellation modal: List of buttons with reasons</Text>
+        </View>
+    </SafeAreaView>
+  )
+}
+
+export default ReasonForCancellation
