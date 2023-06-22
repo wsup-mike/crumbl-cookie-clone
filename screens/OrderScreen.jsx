f6993813b8d56b827286dd779214750506962945
Mike Ramos <michaelangeloram8@gmail.com>
2023-06-22 17:22:44 -0500

    Convert TouchableOp to Button

--- screens/OrderScreen.jsx
+++ screens/OrderScreen.jsx
@@ -1,37 +1,46 @@
-import { View, Text, SafeAreaView, TouchableOpacity } from 'react-native'
+import { View, Text, SafeAreaView, Button } from 'react-native'
 import React from 'react'
 
 
 const OrderScreen = () => {
+
+  const goToPickupLocation = () => {
+
+  }
   return (
     <SafeAreaView className='flex-1 items-center justify-center'>
         <View>
             <Text className='text-3xl text-center'>ORDER SCREEN:5 Options to Order</Text>
-            <TouchableOpacity>
-              <View className='items-center py-4'>
-                <Text>StorePickupOrderingStack</Text>
-              </View>
-            </TouchableOpacity>
-            <TouchableOpacity>
-              <View className='items-center py-4'>
-                <Text>DeliveryOrderingStack</Text>
-              </View>
-            </TouchableOpacity>
-            <TouchableOpacity>
-              <View className='items-center py-4'>
-                <Text>CateringPickupOrderingStack</Text>
-              </View>
-            </TouchableOpacity>
-            <TouchableOpacity>
-              <View className='items-center py-4'>
-                <Text>ShippingOrderingStack</Text>
-              </View>
-            </TouchableOpacity>
-            <TouchableOpacity>
-              <View className='items-center py-4'>
-                <Text>SubscriptionOrderingStack</Text>
-              </View>
-            </TouchableOpacity>
+            <Button
+              title='StorePickupOrderingStack' 
+              onPress={() => {}}
+            />
+             
+            
+            <Button 
+              title='StorePickupOrderingStack' 
+              onPress={() => {}}
+            />
+          
+            
+            <Button 
+              title='StorePickupOrderingStack' 
+              onPress={() => {}}
+            />
+           
+            
+            <Button 
+              title='StorePickupOrderingStack' 
+              onPress={() => {}}
+            />
+        
+            
+            <Button 
+              title='StorePickupOrderingStack' 
+              onPress={() => {}}
+            />
+   
+            
         </View>
     </SafeAreaView>
   )
