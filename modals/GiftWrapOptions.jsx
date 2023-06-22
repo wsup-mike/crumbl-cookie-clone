c8cde48ee59f63c8efe9dc38d4b25bd164155be5
Mike Ramos <michaelangeloram8@gmail.com>
2023-06-22 10:19:48 -0500

    Cr8 GiftWrapOptions modal

---
+++ modals/GiftWrapOptions.jsx
@@ -0,0 +1,14 @@
+import { View, Text, SafeAreaView } from 'react-native'
+import React from 'react'
+
+const GiftWrapOptions = () => {
+  return (
+    <SafeAreaView>    
+        <View>
+            <Text className='text-3xl text-center'>GiftWrapOptions modal: Gift wrapping and bow</Text>
+        </View>
+    </SafeAreaView>
+  )
+}
+
+export default GiftWrapOptions
