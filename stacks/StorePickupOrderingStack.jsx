3c4fa35d6d5607cec51fba8ab8368422d293aa06
Mike Ramos <michaelangeloram8@gmail.com>
2023-06-22 08:57:21 -0500

    Cr8 new stack

---
+++ stacks/StorePickupOrderingStack.jsx
@@ -0,0 +1,12 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+const StorePickupOrderingStack = () => {
+  return (
+    <View>
+      <Text>StorePickupOrderingStack</Text>
+    </View>
+  )
+}
+
+export default StorePickupOrderingStack
