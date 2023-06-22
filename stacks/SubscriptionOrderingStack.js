a64c82b9253ce36827654d8ce6e1b85fa6c4eece
Mike Ramos <michaelangeloram8@gmail.com>
2023-06-22 08:58:14 -0500

    Cr8 new stack

---
+++ stacks/SubscriptionOrderingStack.js
@@ -0,0 +1,12 @@
+import { View, Text } from "react-native";
+import React from "react";
+
+const SubscriptionOrderingStack = () => {
+  return (
+    <View>
+      <Text>SubscriptionOrderingStack</Text>
+    </View>
+  );
+};
+
+export default SubscriptionOrderingStack;
