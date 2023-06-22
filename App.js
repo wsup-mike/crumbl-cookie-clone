233a49e869c36194f7b87eb2056a314fab2b869d
Mike Ramos <michaelangeloram8@gmail.com>
2023-06-21 21:19:08 -0500

    import RootNavigator

--- App.js
+++ App.js
@@ -1,17 +1,18 @@
 import { StatusBar } from "expo-status-bar";
 import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 import HomeScreen from "./screens/HomeScreen";
+import RootNavigator from "./RootNavigator";
 
 const Stack = createNativeStackNavigator();
+const Tab = createBottomTabNavigator();
 
 export default function App() {
   return (
     <NavigationContainer>
       <StatusBar style="auto" />
-      <Stack.Navigator>
-        <Stack.Screen name="Home" component={HomeScreen} />
-      </Stack.Navigator>
+      <RootNavigator />
     </NavigationContainer>
   );
 }
