1348b7eb1eae25cc123db5ebbc1021e9ea1b2d9e
Mike Ramos <michaelangeloram8@gmail.com>
2023-06-22 10:45:50 -0500

    Cr8 AddtionalPaymentOptin modal

---
+++ modals/AdditionalPaymentOption.jsx
@@ -0,0 +1,14 @@
+import { View, Text, SafeAreaView } from 'react-native'
+import React from 'react'
+
+const AdditionalPaymentOption = () => {
+  return (
+    <SafeAreaView className='flex-1 items-center justify-center'>
+        <View>
+            <Text className='text-3xl text-center'>AdditionalPaymentOption modal: Voucher, Gift Cards, Promo Codes</Text>
+        </View>
+    </SafeAreaView>
+  )
+}
+
+export default AdditionalPaymentOption
