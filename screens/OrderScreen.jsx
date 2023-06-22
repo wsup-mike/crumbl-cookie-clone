924d39c2f503e6cf85604add29ea971d08926987
Mike Ramos <michaelangeloram8@gmail.com>
2023-06-21 21:26:42 -0500

    Cr8 OrderScreen

---
+++ screens/OrderScreen.jsx
@@ -0,0 +1,14 @@
+import { View, Text, SafeAreaView} from 'react-native'
+import React from 'react'
+
+const OrderScreen = () => {
+  return (
+    <SafeAreaView className='flex-1 items-center justify-center'>
+        <View>
+            <Text className='text-3xl text-center'>ORDER SCREEN:5 Options to Order</Text>
+        </View>
+    </SafeAreaView>
+  )
+}
+
+export default OrderScreen
