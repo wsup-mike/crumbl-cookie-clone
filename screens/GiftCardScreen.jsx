115aec223d2c494758d29d61ae5e3825dc697ff8
Mike Ramos <michaelangeloram8@gmail.com>
2023-06-21 21:30:19 -0500

    Cr8 GiftCardScreen

---
+++ screens/GiftCardScreen.jsx
@@ -0,0 +1,14 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+const GiftCardScreen = () => {
+  return (
+    <SafeAreaView className='flex-1 items-center justify-center'>
+        <View>
+            <Text className='text-3xl text-center'>Gift Card Screen: Purchase Digital Gift Cards</Text>
+        </View>
+    </SafeAreaView>
+  )
+}
+
+export default GiftCardScreen;
