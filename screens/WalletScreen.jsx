2abe28f16e3315e2bc454a0fafa77b1d02aa58cd
Mike Ramos <michaelangeloram8@gmail.com>
2023-06-21 21:28:14 -0500

    Wallet Scren edit

---
+++ screens/WalletScreen.jsx
@@ -0,0 +1,14 @@
+import { View, Text, SafeAreaView} from 'react-native'
+import React from 'react'
+
+const WalletScreen = () => {
+  return (
+    <SafeAreaView className='flex-1 items-center justify-center'>
+        <View>
+            <Text className='text-3xl text-center'>Wallet Screen:Loyalty Crumbl Cash</Text>
+        </View>
+    </SafeAreaView>
+  )
+}
+
+export default WalletScreen;
