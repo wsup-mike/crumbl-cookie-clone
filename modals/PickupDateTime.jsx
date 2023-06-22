279ad1c0b3f37fdd80cd39e2a48e4d9d0ce6a765
Mike Ramos <michaelangeloram8@gmail.com>
2023-06-22 10:12:18 -0500

    Cr8 PickupDateTime modal

---
+++ modals/PickupDateTime.jsx
@@ -0,0 +1,14 @@
+import { View, Text, SafeAreaView } from 'react-native'
+import React from 'react'
+
+const PickupDateTime = () => {
+  return (
+    <SafeAreaView className='flex-1 items-center justify-center'>
+        <View>
+            <Text className='text-3xl text-center'>PickupDataTime modal: Carry out pickup</Text>
+        </View>
+    </SafeAreaView>
+  )
+}
+
+export default PickupDateTime
