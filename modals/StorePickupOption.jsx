67ecde5966cb45ca7a5567b3873391cf06404543
Mike Ramos <michaelangeloram8@gmail.com>
2023-06-22 21:57:24 -0500

    Test Carryout button - see if need {screen}

--- modals/StorePickupOption.jsx
+++ modals/StorePickupOption.jsx
@@ -10,6 +10,12 @@ const StorePickupOption = () => {
     <SafeAreaView className='flex-1 items-center justify-center'>
         <View>
             <Text className='text-3xl text-center text-black'>StorePickupOption Modal: Carryout or Curbside</Text>
+            <Button title='Carryout' onPress={() => {
+              navigation.navigate('CarryoutOrderingStack')
+            }}/>
+            <Button title='Curbside' onPress={() => {
+              
+            }}/>
         </View>
     </SafeAreaView>
   )
