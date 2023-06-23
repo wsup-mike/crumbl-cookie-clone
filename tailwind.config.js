0f30297fe3c93b6fc7bf0956f244434dc13fcbae
Mike Ramos <michaelangeloram8@gmail.com>
2023-06-22 21:32:41 -0500

    Add modals directory

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,6 +4,7 @@ module.exports = {
     "./App.{js,jsx,ts,tsx}",
     "./screens/**/*.{js,jsx,ts,tsx}",
     "./components/**/*.{js,jsx,ts,tsx}",
+    "./modals/**/*.{js,jsx,ts,tsx}",
   ],
   theme: {
     extend: {},
