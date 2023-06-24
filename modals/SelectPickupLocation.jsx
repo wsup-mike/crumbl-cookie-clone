fb8b27e3e7ea1ef6cc86f57f869c5e36f059fd8a
Mike Ramos <michaelangeloram8@gmail.com>
2023-06-23 23:12:19 -0500

    commit

--- modals/SelectPickupLocation.jsx
+++ modals/SelectPickupLocation.jsx
@@ -1,8 +1,8 @@
-import { View, Text, SafeAreaView, Button, StyleSheet } from 'react-native'
-import React, { useEffect, useState } from 'react'
+import { View, Text, SafeAreaView, Button, Animated } from 'react-native'
+import React, { useState } from 'react'
 import { useNavigation } from '@react-navigation/native'
 import TestingModal from './TestingModal'
-import * as Animatable from 'react-native-animatable';
+
 
 const SelectPickupLocation = () => {
   const [modalVisible, setModalVisible] = useState(false);
@@ -19,10 +19,7 @@ const SelectPickupLocation = () => {
 
   return (
     <SafeAreaView className='flex-1'>
-      <Animatable.View
-        animation={modalVisible ? 'slideInUp' : 'slideOutDown'}
-        style={[tw`flex-1 items-center justify-center`, modalVisible && tw`scale-75 pt-30`]}
-      >
+
         <View className='flex-1 items-center justify-center'>
           <Text className='text-3xl text-center text-black'>SelectPickupLocation Modal</Text>
           <Button 
@@ -39,11 +36,9 @@ const SelectPickupLocation = () => {
             visible={modalVisible}
             onClose={closeModal}
           />
-          <View className='absolute inset-0 bg-black opacity-50' />
+          
         </View>
-      </Animatable.View>
-      {modalVisible && <View style={tw`absolute inset-0 bg-black opacity-50`} />}
-      {modalVisible && <TestingModal visible={modalVisible} onClose={closeModal} />}
+      
     </SafeAreaView>
   )
 }
