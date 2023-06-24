50bf2809e8c41a1af8144be0babad3ecfc4fea13
Mike Ramos <michaelangeloram8@gmail.com>
2023-06-24 14:49:21 -0500

    another bad refactor

--- modals/SelectPickupLocation.jsx
+++ modals/SelectPickupLocation.jsx
@@ -69,70 +69,110 @@
 
 // Start new code here:
 
-import { View, Text, SafeAreaView, Button, Animated } from 'react-native'
-import React, { useState, useRef } from 'react'
-import { useNavigation } from '@react-navigation/native'
-import TestingModal from './TestingModal'
+import React, { useState, useRef } from 'react';
+import { View, Text, SafeAreaView, Button, Animated, StyleSheet } from 'react-native';
+import { useNavigation } from '@react-navigation/native';
+import TestingModal from './TestingModal';
 
 const SelectPickupLocation = () => {
   const [modalVisible, setModalVisible] = useState(false);
   const scaleValue = useRef(new Animated.Value(1)).current;
+  const backgroundOpacity = useRef(new Animated.Value(0)).current;
 
   const navigation = useNavigation();
 
   const openModal = () => {
-    setModalVisible(true)
-  }
+    setModalVisible(true);
+    animateScreenShrink();
+  };
 
   const closeModal = () => {
-    setModalVisible(false)
-  }
+    setModalVisible(false);
+    animateScreenExpand();
+  };
 
   const animateScreenShrink = () => {
-    Animated.timing(scaleValue, {
-      toValue: 0.9,
-      duration: 300,
-      useNativeDriver: true,
-    }).start();
+    Animated.parallel([
+      Animated.timing(scaleValue, {
+        toValue: 0.9,
+        duration: 300,
+        useNativeDriver: true,
+      }),
+      Animated.timing(backgroundOpacity, {
+        toValue: 0.5,
+        duration: 300,
+        useNativeDriver: true,
+      }),
+    ]).start();
   };
 
   const animateScreenExpand = () => {
-    Animated.timing(scaleValue, {
-      toValue: 1,
-      duration: 300,
-      useNativeDriver: true,
-    }).start();
+    Animated.parallel([
+      Animated.timing(scaleValue, {
+        toValue: 1,
+        duration: 300,
+        useNativeDriver: true,
+      }),
+      Animated.timing(backgroundOpacity, {
+        toValue: 0,
+        duration: 300,
+        useNativeDriver: true,
+      }),
+    ]).start();
   };
 
   return (
-    <SafeAreaView style={{ flex: 1 }}>
-      <Animated.View style={[{ flex: 1, transform: [{ scale: scaleValue }] }]}>
-        <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-          <Text style={{ fontSize: 30, marginTop: 8, marginBottom: 6, textAlign: 'center' }}>
-            SelectPickupLocation Modal
-          </Text>
-          <Button
-            title="StorePickupOption"
-            onPress={() => {
-              navigation.navigate('StorePickupOrderingStack', { screen: 'StorePickupOption' })
-            }}
-          />
+    <SafeAreaView style={styles.container}>
+      <Animated.View style={[styles.background, { opacity: backgroundOpacity }]} />
+      <View style={styles.wrapper}>
+        <Animated.View style={[styles.content, { transform: [{ scale: scaleValue }] }]}>
+          <View style={styles.innerContent}>
+            <Text style={styles.title}>SelectPickupLocation Modal</Text>
+            <Button
+              title="StorePickupOption"
+              onPress={() => {
+                navigation.navigate('StorePickupOrderingStack', { screen: 'StorePickupOption' });
+              }}
+            />
 
-          <Button
-            title="Open TestingModal"
-            onPress={openModal}
-          />
-          <TestingModal
-            visible={modalVisible}
-            onClose={closeModal}
-            onModalShow={animateScreenShrink}
-            onModalHide={animateScreenExpand}
-          />
-        </View>
-      </Animated.View>
+            <Button title="Open TestingModal" onPress={openModal} />
+            <TestingModal visible={modalVisible} onClose={closeModal} />
+          </View>
+        </Animated.View>
+      </View>
     </SafeAreaView>
-  )
-}
+  );
+};
 
-export default SelectPickupLocation
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+  background: {
+    ...StyleSheet.absoluteFillObject,
+    backgroundColor: 'black',
+  },
+  wrapper: {
+    flex: 1,
+  },
+  content: {
+    flex: 1,
+    borderTopLeftRadius: 20,
+    borderTopRightRadius: 20,
+    overflow: 'hidden',
+  },
+  innerContent: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  title: {
+    fontSize: 30,
+    marginTop: 8,
+    marginBottom: 6,
+    textAlign: 'center',
+    color: 'white',
+  },
+});
 
+export default SelectPickupLocation;
