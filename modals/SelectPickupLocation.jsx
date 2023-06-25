0b4d941a1f52f0b358bd0f9fb480482ec7a9ce88
Mike Ramos <michaelangeloram8@gmail.com>
2023-06-24 23:11:20 -0500

    commit

--- modals/SelectPickupLocation.jsx
+++ modals/SelectPickupLocation.jsx
@@ -69,16 +69,105 @@
 
 // Start new code here:
 
+// import React, { useState, useRef } from 'react';
+// import { View, Text, SafeAreaView, Button, Animated, StyleSheet } from 'react-native';
+// import { useNavigation } from '@react-navigation/native';
+// import TestingModal from './TestingModal';
+
+// const SelectPickupLocation = () => {
+//   const [modalVisible, setModalVisible] = useState(false);
+//   const scaleValue = useRef(new Animated.Value(1)).current;
+
+//   const navigation = useNavigation();
+
+//   const openModal = () => {
+//     setModalVisible(true);
+//     animateScreenShrink();
+//   };
+
+//   const closeModal = () => {
+//     setModalVisible(false);
+//     animateScreenExpand();
+//   };
+
+//   const animateScreenShrink = () => {
+//     Animated.timing(scaleValue, {
+//       toValue: 0.9,
+//       duration: 300,
+//       useNativeDriver: true,
+//     }).start();
+//   };
+
+//   const animateScreenExpand = () => {
+//     Animated.timing(scaleValue, {
+//       toValue: 1,
+//       duration: 300,
+//       useNativeDriver: true,
+//     }).start();
+//   };
+
+//   const wrapperStyle = {
+//     flex: 1,
+//     borderTopLeftRadius: modalVisible ? 20 : 0,
+//     borderTopRightRadius: modalVisible ? 20 : 0,
+//     overflow: 'hidden',
+//   };
+
+//   return (
+//     <SafeAreaView style={styles.container}>
+//       <Animated.View style={[wrapperStyle, { transform: [{ scale: scaleValue }] }]}>
+        
+//         <View style={styles.body}>
+//           <Text style={styles.title}>SelectPickupLocation Modal</Text>
+//           <Button
+//             title="StorePickupOption"
+//             onPress={() => {
+//               navigation.navigate('StorePickupOrderingStack', { screen: 'StorePickupOption' });
+//             }}
+//           />
+
+//           <Button title="Open TestingModal" onPress={openModal} />
+//           <TestingModal visible={modalVisible} onClose={closeModal} />
+//         </View>
+//       </Animated.View>
+//     </SafeAreaView>
+//   );
+// };
+
+// const styles = StyleSheet.create({
+//   container: {
+//     flex: 1,
+//     backgroundColor: 'black',
+//   },
+//   body: {
+//     flex: 1,
+//     alignItems: 'center',
+//     justifyContent: 'center',
+//     backgroundColor: 'blue',
+//   },
+//   title: {
+//     fontSize: 30,
+//     marginTop: 8,
+//     marginBottom: 6,
+//     textAlign: 'center',
+//     color: 'white',
+//   },
+// });
+
+// export default SelectPickupLocation;
+
+// Start other new code here:
+
 import React, { useState, useRef } from 'react';
-import { View, Text, SafeAreaView, Button, Animated, StyleSheet } from 'react-native';
-import { useNavigation, useHeaderHeight } from '@react-navigation/native';
+import { View, Text, SafeAreaView, Button, Animated } from 'react-native';
+import { useNavigation } from '@react-navigation/native';
 import TestingModal from './TestingModal';
 
 const SelectPickupLocation = () => {
   const [modalVisible, setModalVisible] = useState(false);
   const scaleValue = useRef(new Animated.Value(1)).current;
+
   const navigation = useNavigation();
-  const headerHeight = useHeaderHeight();
 
   const openModal = () => {
     setModalVisible(true);
@@ -113,19 +202,13 @@ const SelectPickupLocation = () => {
     overflow: 'hidden',
   };
 
-  const headerStyle = {
-    height: headerHeight,
-    transform: [{ scale: scaleValue }],
-  };
-
   return (
-    <SafeAreaView style={styles.container}>
+    <SafeAreaView style={{ flex: 1, backgroundColor: 'black' }}>
       <Animated.View style={[wrapperStyle, { transform: [{ scale: scaleValue }] }]}>
-        <View style={[styles.header, headerStyle]}>
-          <Text style={styles.headerTitle}>SelectPickupLocation</Text>
-        </View>
-        <View style={styles.body}>
-          <Text style={styles.title}>SelectPickupLocation Modal</Text>
+        <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center', backgroundColor: 'white' }}>
+          <Text style={{ fontSize: 30, marginTop: 8, marginBottom: 6, textAlign: 'center', color: 'black' }}>
+            SelectPickupLocation Modal
+          </Text>
           <Button
             title="StorePickupOption"
             onPress={() => {
@@ -140,35 +223,5 @@ const SelectPickupLocation = () => {
     </SafeAreaView>
   );
 };
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: 'black',
-  },
-  header: {
-    backgroundColor: 'gray',
-    justifyContent: 'center',
-    alignItems: 'center',
-  },
-  headerTitle: {
-    fontSize: 20,
-    fontWeight: 'bold',
-    color: 'white',
-  },
-  body: {
-    flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
-    backgroundColor: 'blue',
-  },
-  title: {
-    fontSize: 30,
-    marginTop: 8,
-    marginBottom: 6,
-    textAlign: 'center',
-    color: 'white',
-  },
-});
 
 export default SelectPickupLocation;
