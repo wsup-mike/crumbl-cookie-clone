331045fb1398cd51cd204ff3c322338fa300e37a
Mike Ramos <michaelangeloram8@gmail.com>
2023-06-27 11:27:28 -0500

    import useState

--- screens/OrderScreen.jsx
+++ screens/OrderScreen.jsx
@@ -1,5 +1,5 @@
 import { View, Text, SafeAreaView, Button } from 'react-native'
-import React from 'react'
+import React, { useState } from 'react'
 import { useNavigation } from '@react-navigation/native'
 import SelectPickupLocation from '../modals/SelectPickupLocation'
 
