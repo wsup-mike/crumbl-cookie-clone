32bf303ac22390eac3cd6f9637cf27a590913083
Mike Ramos <michaelangeloram8@gmail.com>
2023-06-27 11:14:42 -0500

    another refacot

--- modals/SelectPickupLocation.jsx
+++ modals/SelectPickupLocation.jsx
@@ -1,5 +1,6 @@
+// Refactored with 'classname'
 import React, { useState, useRef } from 'react';
-import { View, Text, SafeAreaView, Button, Animated, StyleSheet } from 'react-native';
+import { View, Text, SafeAreaView, Button, Animated } from 'react-native';
 import { useNavigation } from '@react-navigation/native';
 import TestingModal from './TestingModal';
 
@@ -35,19 +36,15 @@ const SelectPickupLocation = () => {
     }).start();
   };
 
-  const wrapperStyle = {
-    flex: 1,
-    borderTopLeftRadius: modalVisible ? 20 : 0,
-    borderTopRightRadius: modalVisible ? 20 : 0,
-    overflow: 'hidden',
-  };
+  const containerClassName = 'flex-1 bg-black';
+  const bodyClassName = 'flex-1 items-center justify-center bg-blue';
+  const titleClassName = 'text-white text-3xl mt-8 mb-6 text-center';
 
   return (
-    <SafeAreaView style={styles.container}>
-      <Animated.View style={[wrapperStyle, { transform: [{ scale: scaleValue }] }]}>
-        
-        <View style={styles.body}>
-          <Text style={styles.title}>SelectPickupLocation Modal</Text>
+    <SafeAreaView className={containerClassName} style={{ backgroundColor: 'transparent' }}>
+      <Animated.View style={{ flex: 1, transform: [{ scale: scaleValue }] }}>
+        <View className={bodyClassName}>
+          <Text className={titleClassName}>SelectPickupLocation Modal</Text>
           <Button
             title="StorePickupOption"
             onPress={() => {
@@ -63,27 +60,98 @@ const SelectPickupLocation = () => {
   );
 };
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: 'black',
-  },
-  body: {
-    flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
-    backgroundColor: 'blue',
-  },
-  title: {
-    fontSize: 30,
-    marginTop: 8,
-    marginBottom: 6,
-    textAlign: 'center',
-    color: 'white',
-  },
-});
-
 export default SelectPickupLocation;
+
+
+
+// Refactored with 'StyleSheet'
+
+// import React, { useState, useRef } from 'react';
+// import { View, Text, SafeAreaView, Button, Animated, StyleSheet } from 'react-native';
+// import { useNavigation } from '@react-navigation/native';
+// import TestingModal from './TestingModal';
+
+// const SelectPickupLocation = () => {
+//   const [modalVisible, setModalVisible] = useState(false);
+//   const scaleValue = useRef(new Animated.Value(1)).current;
+
+//   const navigation = useNavigation();
+
+//   const openModal = () => {
+//     setModalVisible(true);
+//     animateScreenShrink();
+//   };
+
+//   const closeModal = () => {
+//     setModalVisible(false);
+//     animateScreenExpand();
+//   };
+
+//   const animateScreenShrink = () => {
+//     Animated.timing(scaleValue, {
+//       toValue: 0.9,
+//       duration: 300,
+//       useNativeDriver: true,
+//     }).start();
+//   };
+
+//   const animateScreenExpand = () => {
+//     Animated.timing(scaleValue, {
+//       toValue: 1,
+//       duration: 300,
+//       useNativeDriver: true,
+//     }).start();
+//   };
+
+//   const wrapperStyle = {
+//     flex: 1,
+//     borderTopLeftRadius: modalVisible ? 20 : 0,
+//     borderTopRightRadius: modalVisible ? 20 : 0,
+//     overflow: 'hidden',
+//   };
+
+//   return (
+//     <SafeAreaView style={styles.container}>
+//       <Animated.View style={[wrapperStyle, { transform: [{ scale: scaleValue }] }]}>
+        
+//         <View style={styles.body}>
+//           <Text style={styles.title}>SelectPickupLocation Modal</Text>
+//           <Button
+//             title="StorePickupOption"
+//             onPress={() => {
+//               navigation.navigate('StorePickupOrderingStack', { screen: 'StorePickupOption' });
+//             }}
+//           />
+
+//           <Button title="Open TestingModal" onPress={openModal} />
+//           <TestingModal visible={modalVisible} onClose={closeModal} />
+//         </View>
+//       </Animated.View>
+//     </SafeAreaView>
+//   );
+// };
+
+// const styles = StyleSheet.create({
+//   container: {
+//     flex: 1,
+//     backgroundColor: 'black',
+//   },
+//   body: {
+//     flex: 1,
+//     alignItems: 'center',
+//     justifyContent: 'center',
+//     backgroundColor: 'blue',
+//   },
+//   title: {
+//     fontSize: 30,
+//     marginTop: 8,
+//     marginBottom: 6,
+//     textAlign: 'center',
+//     color: 'white',
+//   },
+// });
+
+// export default SelectPickupLocation;
 
 // // Start other new code here:
 
