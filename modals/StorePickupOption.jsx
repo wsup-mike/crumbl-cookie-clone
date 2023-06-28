a3916115a5a41f9170de06f3ebd832e7f487052c
Mike Ramos <michaelangeloram8@gmail.com>
2023-06-28 08:54:07 -0500

    Wrap Modal around component

--- modals/StorePickupOption.jsx
+++ modals/StorePickupOption.jsx
@@ -1,23 +1,33 @@
 import { View, Text, SafeAreaView, Button } from 'react-native'
 import React from 'react'
 import { useNavigation } from '@react-navigation/native'
+import Modal from 'react-native-modal'
 
 const StorePickupOption = () => {
 
   const navigation = useNavigation();
 
   return (
-    <SafeAreaView className='flex-1 items-center justify-center'>
-        <View>
-            <Text className='text-3xl text-center text-black'>StorePickupOption Modal: Carryout or Curbside</Text>
-            <Button title='Carryout' onPress={() => {
-              navigation.navigate('CarryoutOrderingStack', {screen: 'CarryoutOrderScreen'})
-            }}/>
-            <Button title='Curbside' onPress={() => {
-              navigation.navigate('CurbsideOrderingStack', {screen: 'CurbsideOrderScreen'})
-            }}/>
-        </View>
-    </SafeAreaView>
+    <Modal
+      isVisible={true}
+      backdropOpacity={0.5}
+      onBackdropPress={onClose}
+      onSwipeComplete={onClose}
+      swipeDirection='down'
+      style={{ margin: 0, paddingTop: 30 }}
+    >
+      <SafeAreaView className='flex-1 items-center justify-center'>
+          <View>
+              <Text className='text-3xl text-center text-black'>StorePickupOption Modal: Carryout or Curbside</Text>
+              <Button title='Carryout' onPress={() => {
+                navigation.navigate('CarryoutOrderingStack', {screen: 'CarryoutOrderScreen'})
+              }}/>
+              <Button title='Curbside' onPress={() => {
+                navigation.navigate('CurbsideOrderingStack', {screen: 'CurbsideOrderScreen'})
+              }}/>
+          </View>
+      </SafeAreaView>
+    </Modal>
   )
 }
 
