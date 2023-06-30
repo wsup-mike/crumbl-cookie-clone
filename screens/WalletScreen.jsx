5a8edd7edf073ffb3a3346eda2d9c92208e766a5
Mike Ramos <michaelangeloram8@gmail.com>
2023-06-30 09:12:27 -0500

    import DeliveryModalSTack

--- screens/WalletScreen.jsx
+++ screens/WalletScreen.jsx
@@ -1,5 +1,7 @@
 import { View, Text, SafeAreaView} from 'react-native'
 import React from 'react'
+import DeliveryModalStack from "./stacks/DeliveryModalStack";
+
 
 const WalletScreen = () => {
   return (
