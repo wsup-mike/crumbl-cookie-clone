a8eec67d7f38974588401551a4a85a154255dca6
Mike Ramos <michaelangeloram8@gmail.com>
2023-06-30 09:16:35 -0500

    Add onPress

--- screens/WalletScreen.jsx
+++ screens/WalletScreen.jsx
@@ -1,12 +1,16 @@
 import { View, Text, SafeAreaView, Button} from 'react-native'
 import React from 'react'
+import { useNavigation } from '@react-navigation/native'
 import DeliveryModalStack from '../stacks/DeliveryModalStack'
 
 const WalletScreen = () => {
+  const navigation = useNavigation();
+
   return (
     <SafeAreaView className='flex-1 items-center justify-center'>
         <View>
             <Text className='text-3xl text-center'>Wallet Screen:Loyalty Crumbl Cash</Text>
+            <Button title='Modals' onPress={() => {navigation.navigate('DeliveryModalStack', {screen: 'DeliveryModal1'})}}/>
         </View>
     </SafeAreaView>
   )
