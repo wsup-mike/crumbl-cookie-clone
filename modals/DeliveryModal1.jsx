58c772141346e1e77a7e0dda0a2953a128ab16c8
Mike Ramos <michaelangeloram8@gmail.com>
2023-06-30 09:08:00 -0500

    Set up button and nav

--- modals/DeliveryModal1.jsx
+++ modals/DeliveryModal1.jsx
@@ -1,11 +1,14 @@
-import { View, Text, SafeAreaView } from 'react-native'
+import { View, Text, SafeAreaView, Button } from 'react-native'
 import React from 'react'
+import { useNavigation } from '@react-navigation/native'
 
 const DeliveryModal1 = () => {
+  const navigation = useNavigation();
+
   return (
     <SafeAreaView className='flex-1 items-center justify-center'>
         <View>
-            <Text className='text-3xl text-center'>DeliveryModal1</Text>
+            <Button title='Next Page' onPress={() => {navigation.navigate('DeliveryModal2')}} />
         </View>
     </SafeAreaView>
   )
