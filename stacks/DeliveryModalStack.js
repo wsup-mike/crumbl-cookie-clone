458638dc8212cbea129e40829b871af2eb90739b
Mike Ramos <michaelangeloram8@gmail.com>
2023-06-30 10:26:53 -0500

    Add NavigationContainer implementation

--- stacks/DeliveryModalStack.js
+++ stacks/DeliveryModalStack.js
@@ -1,22 +1,36 @@
 import { View, Text } from "react-native";
 import React from "react";
 import { createStackNavigator } from "@react-navigation/stack";
+import { NavigationContainer } from "@react-navigation/native";
 import DeliveryModal1 from "../modals/DeliveryModal1";
 import DeliveryModal2 from "../modals/DeliveryModal2";
 import DeliveryModal3 from "../modals/DeliveryModal3";
 
-const DeliveryModalStack = () => {
-  const Stack = createStackNavigator();
+const Stack = createStackNavigator();
 
+const ParentNavigator = () => {
   return (
     <Stack.Navigator
-      initialRouteName="DeliveryModal1"
-      // screenOptions={{ headerShown: false }}
+      initialRouteName="DeliveryModalStack"
+      screenOptions={{ headerShown: false }}
     >
-      <Stack.Screen name="DeliveryModal1" component={DeliveryModal1} />
-      <Stack.Screen name="DeliveryModal2" component={DeliveryModal2} />
-      <Stack.Screen name="DeliveryModal3" component={DeliveryModal3} />
+      <Stack.Screen name="DeliveryModalStack" component={DeliveryModalStack} />
     </Stack.Navigator>
+  );
+};
+
+const DeliveryModalStack = () => {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator
+        initialRouteName="DeliveryModal1"
+        // screenOptions={{ headerShown: false }}
+      >
+        <Stack.Screen name="DeliveryModal1" component={DeliveryModal1} />
+        <Stack.Screen name="DeliveryModal2" component={DeliveryModal2} />
+        <Stack.Screen name="DeliveryModal3" component={DeliveryModal3} />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 };
 
