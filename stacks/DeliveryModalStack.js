8ce8d347b7791215bf9be9e9729172dc05992a1e
Mike Ramos <michaelangeloram8@gmail.com>
2023-06-30 12:11:40 -0500

    import NormalScreen

--- stacks/DeliveryModalStack.js
+++ stacks/DeliveryModalStack.js
@@ -4,6 +4,7 @@ import DeliveryModal1 from "../modals/DeliveryModal1";
 import DeliveryModal2 from "../modals/DeliveryModal2";
 import DeliveryModal3 from "../modals/DeliveryModal3";
 import WalletScreen from "../screens/WalletScreen";
+import NormalScreen from "../screens/NormalScreen";
 
 const DeliveryModalStack = () => {
   const Stack = createStackNavigator();
@@ -15,6 +16,7 @@ const DeliveryModalStack = () => {
       screenOptions={{ presentation: "modal" }}
     >
       <Stack.Screen name="WalletScreen" component={WalletScreen} />
+      <Stack.Screen name="NormalScreen" component={NormalScreen} />
       <Stack.Screen name="DeliveryModal1" component={DeliveryModal1} />
       <Stack.Screen name="DeliveryModal2" component={DeliveryModal2} />
       <Stack.Screen name="DeliveryModal3" component={DeliveryModal3} />
