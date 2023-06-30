2b0bfab6c588192086d7176855a5b4bca21cd57d
Mike Ramos <michaelangeloram8@gmail.com>
2023-06-30 09:32:42 -0500

    Remove 'screen' name

--- screens/WalletScreen.jsx
+++ screens/WalletScreen.jsx
@@ -10,7 +10,7 @@ const WalletScreen = () => {
     <SafeAreaView className='flex-1 items-center justify-center'>
         <View>
             <Text className='text-3xl text-center'>Wallet Screen:Loyalty Crumbl Cash</Text>
-            <Button title='Modals' onPress={() => {navigation.navigate('DeliveryModalStack', {screen: 'DeliveryModal1'})}}/>
+            <Button title='Modals' onPress={() => {navigation.navigate('DeliveryModalStack')}}/>
         </View>
     </SafeAreaView>
   )
