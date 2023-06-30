bd245f2974270f7270ee3cefa2a231cc257cbd1d
Mike Ramos <michaelangeloram8@gmail.com>
2023-06-30 10:42:16 -0500

    Remove ParentNavigator implementation

--- stacks/DeliveryModalStack.js
+++ stacks/DeliveryModalStack.js
@@ -1,25 +1,12 @@
-import { View, Text } from "react-native";
 import React from "react";
 import { createStackNavigator } from "@react-navigation/stack";
-import { NavigationContainer } from "@react-navigation/native";
 import DeliveryModal1 from "../modals/DeliveryModal1";
 import DeliveryModal2 from "../modals/DeliveryModal2";
 import DeliveryModal3 from "../modals/DeliveryModal3";
 
-const Stack = createStackNavigator();
-
-const ParentNavigator = () => {
-  return (
-    <Stack.Navigator
-      initialRouteName="DeliveryModalStack"
-      screenOptions={{ headerShown: false }}
-    >
-      <Stack.Screen name="DeliveryModalStack" component={DeliveryModalStack} />
-    </Stack.Navigator>
-  );
-};
-
 const DeliveryModalStack = () => {
+  const Stack = createStackNavigator();
+
   return (
     <NavigationContainer>
       <Stack.Navigator
