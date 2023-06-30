bdf57c1a871b4770fa7afdc9d5eef3b021712ad7
Mike Ramos <michaelangeloram8@gmail.com>
2023-06-30 08:39:58 -0500

    Cr8 DeliveryModalStack stack

---
+++ stacks/DeliveryModalStack.js
@@ -0,0 +1,23 @@
+import { View, Text } from "react-native";
+import React from "react";
+import { createStackNavigator } from "@react-navigation/stack";
+import { DeliveryModal1 } from "../modals/DeliveryModal1";
+import { DeliveryModal2 } from "../modals/DeliveryModal2";
+import { DeliveryModal3 } from "../modals/DeliveryModal3";
+
+const DeliveryModalStack = () => {
+  const Stack = createStackNavigator();
+
+  return (
+    <Stack.Navigator
+      initialRouteName="DeliveryModal1"
+      screenOptions={{ headerShown: false }}
+    >
+      <Stack.Screen name="DeliveryModal1" component={DeliveryModal1} />
+      <Stack.Screen name="DeliveryModal2" component={DeliveryModal2} />
+      <Stack.Screen name="DeliveryModal3" component={DeliveryModal3} />
+    </Stack.Navigator>
+  );
+};
+
+export default DeliveryModalStack;
