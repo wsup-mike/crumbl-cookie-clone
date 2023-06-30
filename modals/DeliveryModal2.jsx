695c052b387a26097e30a8ce87cba76d1e3d118b
Mike Ramos <michaelangeloram8@gmail.com>
2023-06-30 08:29:00 -0500

    DeliveryModal2

---
+++ modals/DeliveryModal2.jsx
@@ -0,0 +1,14 @@
+import { View, Text, SafeAreaView } from 'react-native'
+import React from 'react'
+
+const DeliveryModal2 = () => {
+  return (
+    <SafeAreaView className='flex-1 items-center justify-center'>
+        <View>
+            <Text className='text-3xl text-center'>DeliveryModal2</Text>
+        </View>
+    </SafeAreaView>
+  )
+}
+
+export default DeliveryModal2
