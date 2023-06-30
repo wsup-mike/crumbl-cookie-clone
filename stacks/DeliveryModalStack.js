ebac41041b81e42f04e92ecc73afc90e6f721c85
Mike Ramos <michaelangeloram8@gmail.com>
2023-06-30 12:33:53 -0500

    Add cardStyleInterpolater

--- stacks/DeliveryModalStack.js
+++ stacks/DeliveryModalStack.js
@@ -1,5 +1,8 @@
 import React from "react";
-import { createStackNavigator } from "@react-navigation/stack";
+import {
+  CardStyleInterpolators,
+  createStackNavigator,
+} from "@react-navigation/stack";
 import DeliveryModal1 from "../modals/DeliveryModal1";
 import DeliveryModal2 from "../modals/DeliveryModal2";
 import DeliveryModal3 from "../modals/DeliveryModal3";
@@ -9,14 +12,52 @@ import NormalScreen from "../screens/NormalScreen";
 const DeliveryModalStack = () => {
   const Stack = createStackNavigator();
 
+  const horizontalAnimation = {
+    cardStyleInterpolator: ({ current, layouts }) => {
+      return {
+        cardStyle: {
+          transform: [
+            {
+              translateX: current.progress.interpolate({
+                inputRange: [0, 1],
+                outputRange: [layouts.screen.width, 0],
+              }),
+            },
+          ],
+        },
+      };
+    },
+  };
+
   return (
     <Stack.Navigator
       initialRouteName="WalletScreen"
       // screenOptions={{ headerShown: false }}
-      screenOptions={{ presentation: "modal" }}
+      screenOptions={{
+        presentation: "modal",
+      }}
     >
       <Stack.Screen name="WalletScreen" component={WalletScreen} />
-      <Stack.Screen name="NormalScreen" component={NormalScreen} />
+      <Stack.Screen
+        name="NormalScreen"
+        component={NormalScreen}
+        options={{
+          cardStyleInterpolator: ({ current, layouts }) => {
+            return {
+              cardStyle: {
+                transform: [
+                  {
+                    translateX: current.progress.interpolate({
+                      inputRange: [0, 1],
+                      outputRange: [layouts.screen.width, 0],
+                    }),
+                  },
+                ],
+              },
+            };
+          },
+        }}
+      />
       <Stack.Screen name="DeliveryModal1" component={DeliveryModal1} />
       <Stack.Screen name="DeliveryModal2" component={DeliveryModal2} />
       <Stack.Screen name="DeliveryModal3" component={DeliveryModal3} />
