e0f38344f3050b91507f68611dc36fa767e2c03b
Mike Ramos <michaelangeloram8@gmail.com>
2023-06-30 11:00:18 -0500

    Add WalletScreen

--- stacks/DeliveryModalStack.js
+++ stacks/DeliveryModalStack.js
@@ -3,15 +3,17 @@ import { createStackNavigator } from "@react-navigation/stack";
 import DeliveryModal1 from "../modals/DeliveryModal1";
 import DeliveryModal2 from "../modals/DeliveryModal2";
 import DeliveryModal3 from "../modals/DeliveryModal3";
+import WalletScreen from "../screens/WalletScreen";
 
 const DeliveryModalStack = () => {
   const Stack = createStackNavigator();
 
   return (
     <Stack.Navigator
-      initialRouteName="DeliveryModal1"
+      initialRouteName="WalletScreen"
       // screenOptions={{ headerShown: false }}
     >
+      <Stack.Screen name="WalletScreen" component={WalletScreen} />
       <Stack.Screen name="DeliveryModal1" component={DeliveryModal1} />
       <Stack.Screen name="DeliveryModal2" component={DeliveryModal2} />
       <Stack.Screen name="DeliveryModal3" component={DeliveryModal3} />
