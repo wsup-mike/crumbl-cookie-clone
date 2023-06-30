25c70f8d5d69d553b39cbedeef32cc639de11107
Mike Ramos <michaelangeloram8@gmail.com>
2023-06-30 12:11:28 -0500

    import and navigate to NormalScreen

--- screens/WalletScreen.jsx
+++ screens/WalletScreen.jsx
@@ -1,6 +1,7 @@
 import { View, Text, SafeAreaView, Button} from 'react-native'
 import React from 'react'
 import { useNavigation } from '@react-navigation/native'
+import NormalScreen from './NormalScreen'
 
 const WalletScreen = () => {
   const navigation = useNavigation();
@@ -10,7 +11,7 @@ const WalletScreen = () => {
         <View>
             <Text className='text-3xl text-center'>Wallet Screen:Loyalty Crumbl Cash</Text>
             <Button title='Modals as Stack Animation' onPress={() => navigation.navigate('DeliveryModal1')}/>
-            <Button title='Screen Only' onPress={() => navigation.navigate('DeliveryModal1')}/>
+            <Button title='Screen Only' onPress={() => navigation.navigate('NormalScreen')}/>
         </View>
     </SafeAreaView>
   )
