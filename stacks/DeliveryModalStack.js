9592a19a1b2ea91a2f9e1f06ebfcf31ed974ecdd
Mike Ramos <michaelangeloram8@gmail.com>
2023-06-30 22:46:17 -0500

    import CardStyleInterpolators

--- stacks/DeliveryModalStack.js
+++ stacks/DeliveryModalStack.js
@@ -1,5 +1,8 @@
 import React from "react";
-import { createStackNavigator } from "@react-navigation/stack";
+import {
+  createStackNavigator,
+  CardStyleInterpolators,
+} from "@react-navigation/stack";
 import horizontalAnimation from "../components/horizontalAnimation";
 import DeliveryModal1 from "../modals/DeliveryModal1";
 import DeliveryModal2 from "../modals/DeliveryModal2";
