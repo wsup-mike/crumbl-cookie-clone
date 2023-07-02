92bd74dab1f1b6c19a8240b6eddff264611527ff
Mike Ramos <michaelangeloram8@gmail.com>
2023-07-02 00:18:46 -0500

    Text center

--- modals/MenuModal1.jsx
+++ modals/MenuModal1.jsx
@@ -16,11 +16,9 @@ const MenuModal1 = () => {
     }
         
     return (
-        <SafeAreaView className='flex-1 items-center justify-center bg-white rounded-t-xl p-8'>
-            <View
-            
-            >
-                <Text>MenuModal1</Text>
+        <SafeAreaView className='flex-1 items-center justify-center bg-yellow-300 mt-10 rounded-t-3xl'>
+            <View >
+                <Text className='text-center'>MenuModal1</Text>
                 <Button title='Close' onPress={closeModal} />
                 <Button title='Next Again!' onPress={nextModal} />
             </View>
