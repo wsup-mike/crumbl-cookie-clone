80f938384087268cfb98727f2110ba1629473587
Mike Ramos <michaelangeloram8@gmail.com>
2023-07-02 00:19:05 -0500

    Add Button and closeModal

--- modals/MenuModal2.jsx
+++ modals/MenuModal2.jsx
@@ -1,11 +1,18 @@
-import { View, Text, SafeAreaView } from 'react-native'
+import { View, Text, SafeAreaView, Button } from 'react-native'
 import React from 'react'
+import { useNavigation } from '@react-navigation/native';
 
 const MenuModal2 = () => {
+    const navigation = useNavigation();
+
+    const closeModal = () => {
+        navigation.goBack();
+    }
   return (
-    <SafeAreaView className='flex-1 items-center justify-center'>
-        <View>
+    <SafeAreaView className='flex-1 items-center justify-center bg-yellow-300 mt-10 rounded-t-3xl'>
+        <View >
             <Text>MenuModal2</Text>
+            <Button title='Close' onPress={closeModal} />
         </View>
     </SafeAreaView>
   )
