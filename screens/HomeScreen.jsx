b712811213a3bdeba5804a9663bb3f4c1906f542
Mike Ramos <michaelangeloram8@gmail.com>
2023-07-01 22:07:50 -0500

    Add Button

--- screens/HomeScreen.jsx
+++ screens/HomeScreen.jsx
@@ -1,11 +1,13 @@
 import { View, Text, ScrollView, SafeAreaView } from 'react-native'
 import React from 'react'
+import { Button } from 'react-native';
 
 const HomeScreen = () => {
   return (
     <SafeAreaView className='flex-1 items-center justify-center'>
         <View>
             <Text className='text-3xl text-center'>HOME SCREEN: The Mutha Frikin Crumbl Cookie Clone App</Text>
+            <Button title='MenuModal1' onPress={()=> {}}/>
         </View>
     </SafeAreaView>
   )
