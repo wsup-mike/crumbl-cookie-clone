287f6b6e8d5bfef433b9bc21baede67d0d22a65e
Mike Ramos <michaelangeloram8@gmail.com>
2023-07-01 22:28:54 -0500

    import Animatable

--- modals/MenuModal1.jsx
+++ modals/MenuModal1.jsx
@@ -1,6 +1,7 @@
 import { View, Text, SafeAreaView, Button } from 'react-native'
 import React from 'react'
 import { useNavigation } from '@react-navigation/native';
+import * as Animatable from 'react-native-animatable'
 
 const MenuModal1 = () => {
     const navigation = useNavigation()
