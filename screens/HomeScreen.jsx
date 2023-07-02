ae04ac81931f5f5c0990cf789a167cf43024b46e
Mike Ramos <michaelangeloram8@gmail.com>
2023-07-01 22:08:48 -0500

    import and implement useNavigation for navigation

--- screens/HomeScreen.jsx
+++ screens/HomeScreen.jsx
@@ -1,8 +1,11 @@
 import { View, Text, ScrollView, SafeAreaView } from 'react-native'
 import React from 'react'
 import { Button } from 'react-native';
+import { useNavigation } from '@react-navigation/native';
 
 const HomeScreen = () => {
+  const navigation = useNavigation();
+  
   return (
     <SafeAreaView className='flex-1 items-center justify-center'>
         <View>
