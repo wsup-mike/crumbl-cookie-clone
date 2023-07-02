835f516fb06dc0f8f27b938fed402832c7d1f844
Mike Ramos <michaelangeloram8@gmail.com>
2023-07-01 23:39:14 -0500

    import MenuModal1 module

--- screens/HomeScreen.jsx
+++ screens/HomeScreen.jsx
@@ -1,18 +1,32 @@
-import { View, Text, ScrollView, SafeAreaView } from 'react-native'
-import React from 'react'
-import { Button } from 'react-native';
+import { View, Text, Button, SafeAreaView } from 'react-native'
+import React, { useState } from 'react'
 import { useNavigation } from '@react-navigation/native';
+import Modal from 'react-native-modal'
+import MenuModal1 from '../modals/MenuModal1';
+
 
 const HomeScreen = () => {
+  const [modalVisible, setModalVisible] = useState(false)
+
   const navigation = useNavigation();
 
+  const openModal = () => {
+    setModalVisible(true)
+  }
+
+  const closeModal = () => {
+    setModalVisible(false)
+  }
+
   return (
-    <SafeAreaView className='flex-1 items-center justify-center'>
-        <View>
-            <Text className='text-3xl text-center'>HOME SCREEN: The Mutha Frikin Crumbl Cookie Clone App</Text>
-            <Button title='Next' onPress={()=> {navigation.navigate('MenuModal1')}}/>
-        </View>
-    </SafeAreaView>
+    
+      <SafeAreaView className='flex-1 items-center justify-center'>
+          <View>
+              <Text className='text-3xl text-center'>HOME SCREEN: The Mutha Frikin Crumbl Cookie Clone App</Text>
+              <Button title='Next' onPress={()=> {navigation.navigate('MenuModal1')}}/>
+          </View>
+      </SafeAreaView>
+    
   )
 }
 
