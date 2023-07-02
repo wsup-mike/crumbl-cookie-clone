c47d74c6dac6dd0964e80e2e515f708bf89b15b8
Mike Ramos <michaelangeloram8@gmail.com>
2023-07-01 22:45:34 -0500

    Apply sliding modal on MenuModal1

--- modals/MenuModal1.jsx
+++ modals/MenuModal1.jsx
@@ -7,12 +7,24 @@ const MenuModal1 = () => {
     const navigation = useNavigation()
         
     return (
-        <SafeAreaView className='flex-1 items-center justify-center'>
-            <View>
-                <Text>MenuModal1</Text>
-                <Button title='Next Again!' onPress={() => {navigation.navigate('MenuModal2')}} />
-            </View>
-        </SafeAreaView>
+        <Animatable.View
+            animation='slideInUp'
+            style={{ 
+                flex: 1, 
+                backgroundColor: 'green', 
+                borderTopLeftRadius: 20, 
+                borderTopRightRadius: 20, 
+                marginTop: 40,
+                elevation: 5,
+            }}
+        >
+            <SafeAreaView className='flex-1 items-center justify-center'>
+                <View>
+                    <Text>MenuModal1</Text>
+                    <Button title='Next Again!' onPress={() => {navigation.navigate('MenuModal2')}} />
+                </View>
+            </SafeAreaView>
+        </Animatable.View>
     )
 }
 
