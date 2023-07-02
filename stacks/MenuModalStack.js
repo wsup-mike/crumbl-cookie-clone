3e203defb376622183b430d3b08645cda3c8a68b
Mike Ramos <michaelangeloram8@gmail.com>
2023-07-01 21:56:52 -0500

    Implement Stack Navigator

--- stacks/MenuModalStack.js
+++ stacks/MenuModalStack.js
@@ -1,7 +1,10 @@
 import { View, Text } from "react-native";
 import React from "react";
+import { createStackNavigator } from "@react-navigation/stack";
 
 const MenuModalStack = () => {
+  const Stack = createStackNavigator();
+
   return (
     <View>
       <Text>MenuModalStack</Text>
