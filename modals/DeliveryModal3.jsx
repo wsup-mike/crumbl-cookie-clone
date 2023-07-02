87712d53f2b763ac921e571c3b4ffc9b664b33d5
Mike Ramos <michaelangeloram8@gmail.com>
2023-07-02 12:22:12 -0500

    import useNavigation for nav object

--- modals/DeliveryModal3.jsx
+++ modals/DeliveryModal3.jsx
@@ -1,8 +1,11 @@
 import { View, Text, SafeAreaView, Button } from 'react-native'
 import React from 'react'
+import { useNavigation } from '@react-navigation/native'
 
 
 const DeliveryModal3 = () => {
+  const navigation = useNavigation();
+  
   return (
     <SafeAreaView className='flex-1 items-center justify-center'>
         <View>
