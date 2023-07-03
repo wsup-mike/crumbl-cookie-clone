5145d0c64e17eb975d34f46188f57259efe8dcc2
Mike Ramos <michaelangeloram8@gmail.com>
2023-07-03 09:21:25 -0500

    Refactor

--- modals/SelectPickupLocation.jsx
+++ modals/SelectPickupLocation.jsx
@@ -2,36 +2,44 @@
 import React, { useState } from 'react';
 import { View, Text, SafeAreaView, Button } from 'react-native';
 import { useNavigation } from '@react-navigation/native';
-import Modal from 'react-native-modal'
+// import Modal from 'react-native-modal'
 // import TestingModal from './TestingModal';
-import StorePickupOption from './StorePickupOption';
+// import StorePickupOption from './StorePickupOption';
 
 
 const SelectPickupLocation = ({ visible, onClose }) => {
-  const [modalVisible, setModalVisible] = useState(false);
+  // const [modalVisible, setModalVisible] = useState(false);
 
   const navigation = useNavigation();
 
-  const openModal = () => {
-    setModalVisible(true);
-  };
+  // const openModal = () => {
+  //   setModalVisible(true);
+  // };
 
-  const closeModal = () => {
-    setModalVisible(false)
-    onClose();
-  };
+  // const closeModal = () => {
+  //   setModalVisible(false)
+  //   onClose();
+  // };
   
   return (
-    <Modal
-      isVisible={visible}
-      backdropOpacity={0.5}
-      // onBackdropPress={onClose}
-      onSwipeComplete={closeModal}
-      swipeDirection='down'
-      style={{ margin: 0, paddingTop: 30 }}
-    >
+    // <Modal
+    //   isVisible={visible}
+    //   backdropOpacity={0.5}
+    //   // onBackdropPress={onClose}
+    //   onSwipeComplete={closeModal}
+    //   swipeDirection='down'
+    //   style={{ margin: 0, paddingTop: 30 }}
+    // >
       <SafeAreaView className='flex-1 bg-white rounded-t-3xl items-center justify-center'>
-          {
+          <View>
+            <Text className='text-3xl text-center'>SelectPickupLocation Modal</Text>
+            <Button 
+              title='StorePickupOption'
+              onPress={openModal}              
+            /> 
+          </View>
+          
+          {/* {
             modalVisible ? (
               <View>
                 <Text className='text-3xl text-center'>SelectPickupLocation Modal</Text>
@@ -43,9 +51,9 @@ const SelectPickupLocation = ({ visible, onClose }) => {
             ) : (
               <StorePickupOption  onClose={closeModal} />
             )
-          }
+          } */}
       </SafeAreaView>
-    </Modal>
+    // </Modal>
   );
 };
 
