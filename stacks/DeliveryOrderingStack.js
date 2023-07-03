9e8b17c6ee502726a0b0937f7eb9e736de680a13
Mike Ramos <michaelangeloram8@gmail.com>
2023-07-03 09:52:09 -0500

    Create Stack object

--- stacks/DeliveryOrderingStack.js
+++ stacks/DeliveryOrderingStack.js
@@ -3,6 +3,8 @@ import React from "react";
 import { createStackNavigator } from "@react-navigation/stack";
 import YourAddresses from "../modals/YourAddresses";
 
+const Stack = createStackNavigator();
+
 const DeliveryOrderingStack = () => {
   return (
     <View>
