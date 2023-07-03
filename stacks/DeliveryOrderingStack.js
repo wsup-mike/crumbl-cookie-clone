fb847f12acd9bc0b6c4c2c85b6e6e5d98c8ae9b1
Mike Ramos <michaelangeloram8@gmail.com>
2023-07-03 11:55:32 -0500

    Add new screen: CreateNewAddress

--- stacks/DeliveryOrderingStack.js
+++ stacks/DeliveryOrderingStack.js
@@ -2,6 +2,7 @@ import { View, Text } from "react-native";
 import React from "react";
 import { createStackNavigator } from "@react-navigation/stack";
 import YourAddresses from "../modals/YourAddresses";
+import CreateNewAddress from "../modals/CreateNewAddress";
 
 const Stack = createStackNavigator();
 
@@ -11,10 +12,10 @@ const DeliveryOrderingStack = () => {
       initialRouteName="YourAddresses"
       screenOptions={{
         headerShown: false,
-        presentation: "modal",
       }}
     >
       <Stack.Screen name="YourAddresses" component={YourAddresses} />
+      <Stack.Screen name="CreateNewAddress" component={CreateNewAddress} />
     </Stack.Navigator>
   );
 };
