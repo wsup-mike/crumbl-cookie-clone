63718d40cd11760578eb60ce656d1652d90ae610
Mike Ramos <michaelangeloram8@gmail.com>
2023-07-03 16:27:26 -0500

    New commit

--- stacks/OrderStack.js
+++ stacks/OrderStack.js
@@ -15,7 +15,7 @@ const OrderStack = () => {
       initialRouteName="OrderScreen"
       screenOptions={{
         headerShown: false,
-        presentation: "card",
+        presentation: "modal",
       }}
     >
       <Stack.Screen name="OrderScreen" component={OrderScreen} options={{}} />
