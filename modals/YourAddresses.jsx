a1ec2c3cbab633bc93a17f674d02f99b2bab6328
Mike Ramos <michaelangeloram8@gmail.com>
2023-07-03 09:49:34 -0500

    Cr8 YourAddresses modal

---
+++ modals/YourAddresses.jsx
@@ -0,0 +1,20 @@
+import { View, Text, SafeAreaView, Button } from 'react-native'
+import React from 'react'
+import { useNavigation } from '@react-navigation/native'
+
+const YourAddresses = () => {
+    const navigation = useNavigation();
+    
+    return (
+        <SafeAreaView className='flex-1 bg-white rounded-t-3xl items-center justify-center'>
+            <View>
+                <Text className='text-3xl text-center text-black'>YourAddresses Modal</Text>
+                <Button title='Carryout' onPress={() => {
+                    navigation.navigate('CreateNewAddress')
+                }}/>
+            </View>
+        </SafeAreaView>
+    )
+}
+
+export default YourAddresses
