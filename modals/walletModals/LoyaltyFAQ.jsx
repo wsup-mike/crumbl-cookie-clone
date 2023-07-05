554c9ae4193103369b854b57fc141749086d8d81
Mike Ramos <michaelangeloram8@gmail.com>
2023-07-05 16:28:58 -0500

    Cr8 LoyaltyFAQ modal

---
+++ modals/walletModals/LoyaltyFAQ.jsx
@@ -0,0 +1,19 @@
+import { View, Text, Button, SafeAreaView } from 'react-native'
+import React from 'react'
+import { useNavigation } from '@react-navigation/native'
+
+const LoyaltyFAQ = () => {
+
+    const navigation = useNavigation();
+  
+    return (
+        <SafeAreaView className='flex-1 items-center justify-center'>
+            <View>
+                <Text className='text-3xl text-center'>Loyalty FAQ modal</Text>
+                <Button title='' onPress={() => navigation.navigate('')}/>
+            </View>
+        </SafeAreaView>
+    )
+}
+
+export default LoyaltyFAQ
