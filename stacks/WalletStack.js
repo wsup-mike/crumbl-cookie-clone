e2431d5dfefc8122c9b6d658d2ab134c49c2229e
Mike Ramos <michaelangeloram8@gmail.com>
2023-07-05 15:48:38 -0500

    Cr8 WalletStack with createStackNav

---
+++ stacks/WalletStack.js
@@ -0,0 +1,13 @@
+import { View, Text } from "react-native";
+import React from "react";
+import { createStackNavigator } from "@react-navigation/stack";
+
+const WalletStack = () => {
+  return (
+    <View>
+      <Text>WalletStack</Text>
+    </View>
+  );
+};
+
+export default WalletStack;
