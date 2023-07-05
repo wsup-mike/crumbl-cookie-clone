a51071a0f737ea3e4b2ff47103a98f3e12385b4c
Mike Ramos <michaelangeloram8@gmail.com>
2023-07-05 16:33:21 -0500

    import and implement navigation object

--- modals/walletModals/CrumblCash.jsx
+++ modals/walletModals/CrumblCash.jsx
@@ -1,17 +1,18 @@
 import { View, Text, Button, SafeAreaView } from 'react-native'
 import React from 'react'
+
 import { useNavigation } from '@react-navigation/native'
 
 const CrumblCash = () => {
 
+    const navigation = useNavigation();
 
-    <SafeAreaView className='flex-1 items-center justify-center bg-purple-200'>
-    <View>
-        <Text className='text-3xl text-center'>Wallet Screen:Loyalty Crumbl Cash</Text>
-        <Button title='Modals as Stack Animation' onPress={() => navigation.navigate('DeliveryModal1')}/>
-        <Button title='Screen Only' onPress={() => navigation.navigate('NormalScreen')}/>
-    </View>
-</SafeAreaView>
+    <SafeAreaView className='flex-1 items-center justify-center'>
+        <View>
+            <Text className='text-3xl text-center'>Crumbl Cash modal</Text>
+            <Button title='' onPress={() => navigation.navigate('')} />
+        </View>
+    </SafeAreaView>
 }
 
 export default CrumblCash
