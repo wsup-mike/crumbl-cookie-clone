ccfd731979e684c5336b59d2e8c69ab1bcafc530
Mike Ramos <michaelangeloram8@gmail.com>
2023-07-06 10:45:58 -0500

    Change bg color

--- screens/GiftCardScreen.jsx
+++ screens/GiftCardScreen.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 
 const GiftCardScreen = () => {
   return (
-    <SafeAreaView className='flex-1 items-center justify-center'>
+    <SafeAreaView className='flex-1 items-center justify-center bg-orange-500'>
         <View>
             <Text className='text-3xl text-center'>Gift Card Screen: Purchase Digital Gift Cards</Text>
         </View>
