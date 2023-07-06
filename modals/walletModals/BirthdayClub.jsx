88c5cbd3f2447238fa9ee655a89e76d3024523dd
Mike Ramos <michaelangeloram8@gmail.com>
2023-07-05 22:00:03 -0500

    Cr8 BirthdayClub modal

---
+++ modals/walletModals/BirthdayClub.jsx
@@ -0,0 +1,13 @@
+import { View, Text, SafeAreaView, Button } from 'react-native'
+import React from 'react'
+
+const BirthdayClub = () => {
+  return (
+    <SafeAreaView className='flex-1 items-center justify-center' >
+           <Text className='text-3xl text-center'>Birthday Club modal</Text>
+           <Text className='text-2xl text-center'>Let's celebrate your birthday!</Text>
+    </SafeAreaView>
+  )
+}
+
+export default BirthdayClub
