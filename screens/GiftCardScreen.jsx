81c45ec5b177194e00792ed0902653ab43227c50
Mike Ramos <michaelangeloram8@gmail.com>
2023-07-06 11:09:16 -0500

    add button for Selected Gift Card

--- screens/GiftCardScreen.jsx
+++ screens/GiftCardScreen.jsx
@@ -1,11 +1,15 @@
-import { View, Text, SafeAreaView } from 'react-native'
+import { View, Text, SafeAreaView, Button } from 'react-native'
 import React from 'react'
+import { useNavigation } from '@react-navigation/native';
 
 const GiftCardScreen = () => {
+  const navigation = useNavigation();
+
   return (
     <SafeAreaView className='flex-1 items-center justify-center bg-orange-500'>
         <View>
             <Text className='text-3xl text-center'>Gift Card Screen: Purchase Digital Gift Cards</Text>
+            <Button title='Gift Card Design #1' onPress={() => navigation.navigate('GiftCardSelectedScreen')} />
         </View>
     </SafeAreaView>
   )
