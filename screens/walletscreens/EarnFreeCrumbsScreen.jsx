f05fc53f068de56b86e0a53c9f5bb11dac8755af
Mike Ramos <michaelangeloram8@gmail.com>
2023-07-05 23:07:19 -0500

    Add all Buttons for EarnFreeCrumbs

--- screens/walletscreens/EarnFreeCrumbsScreen.jsx
+++ screens/walletscreens/EarnFreeCrumbsScreen.jsx
@@ -12,6 +12,16 @@ const EarnFreeCrumbsScreen = () => {
                 <Button title='Refer a friend' onPress={() => navigation.navigate('ReferFriend')}/>
                 <Button title='Join birthday club' onPress={() => navigation.navigate('BirthdayClub')}/>
                 <Button title='Sign up for deals' onPress={() => navigation.navigate('SignUpDeals')} />
+                <Button title='Review us on Facebook' onPress={() => navigation.navigate('FacebookReview')} />
+                <Button title='Review us on Google' onPress={() => navigation.navigate('GoogleReview')} />
+                <Button title='Follow us on LinkedIn' onPress={() => navigation.navigate('LinkedInFollow')} />
+                <Button title='Follow us on Pinterest' onPress={() => navigation.navigate('PinterestFollow')} />
+                <Button title='Follow us on TikTok' onPress={() => navigation.navigate('TikTokFollow')} />
+                <Button title='Follow us on Twitter' onPress={() => navigation.navigate('TwitterFollow')} />
+                <Button title='Follow us on YouTube' onPress={() => navigation.navigate('YouTubeFollow')} />
+                <Button title='Rate our app' onPress={() => navigation.navigate('RateApp')} />
+                <Button title='Follow us on Facebook' onPress={() => navigation.navigate('FacebookFollow')} />
+                <Button title='Follow us on Instagram' onPress={() => navigation.navigate('InstagramFollow')} />
             </View>
         </SafeAreaView>
   )
