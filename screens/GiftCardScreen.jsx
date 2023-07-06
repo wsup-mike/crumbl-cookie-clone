61d026afdd8393609147103fac2e3f6157275b5f
Mike Ramos <michaelangeloram8@gmail.com>
2023-07-06 11:12:31 -0500

    pink backtground

--- screens/GiftCardScreen.jsx
+++ screens/GiftCardScreen.jsx
@@ -6,7 +6,7 @@ const GiftCardScreen = () => {
   const navigation = useNavigation();
 
   return (
-    <SafeAreaView className='flex-1 items-center justify-center bg-orange-500'>
+    <SafeAreaView className='flex-1 items-center justify-center bg-pink-300'>
         <View>
             <Text className='text-3xl text-center'>Gift Card Screen: Purchase Digital Gift Cards</Text>
             <Button title='Gift Card Design #1' onPress={() => navigation.navigate('GiftCardSelectedScreen')} />
