47b5f37634a31264241f96d19468362c0fcd4be7
Mike Ramos <michaelangeloram8@gmail.com>
2023-07-05 22:53:13 -0500

    Cr8 YouTubeFollow modal

---
+++ modals/walletModals/YouTubeFollow.jsx
@@ -0,0 +1,12 @@
+import { View, Text, SafeAreaView } from 'react-native'
+import React from 'react'
+
+const YouTubeFollow = () => {
+  return (
+    <SafeAreaView className='flex-1 items-center justify-center' >
+           <Text className='text-3xl text-center'>Follow us on YouTube</Text>
+    </SafeAreaView>
+  )
+}
+
+export default YouTubeFollow
