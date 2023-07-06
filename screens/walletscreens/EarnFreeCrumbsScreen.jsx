f0dd7faaccadf84403e1157d37b1906628b3f9a7
Mike Ramos <michaelangeloram8@gmail.com>
2023-07-05 21:34:52 -0500

    Cr8 EArnFreeCrumbs

--- screens/walletscreens/EarnFreeCrumbsScreen.jsx
+++ screens/walletscreens/EarnFreeCrumbsScreen.jsx
@@ -1,11 +1,19 @@
-import { View, Text } from 'react-native'
+import { View, Text, SafeAreaView, Button } from 'react-native'
 import React from 'react'
+import { useNavigation } from '@react-navigation/native'
 
 const EarnFreeCrumbsScreen = () => {
-  return (
-    <View>
-      <Text>EarnFreeCrumbsScreen</Text>
-    </View>
+    const navigation = useNavigation();
+
+    return (
+        <SafeAreaView className='flex-1 items-center justify-center bg-purple-200'>
+            <View>
+                <Text className='text-3xl text-center'>Earn Free Crumbs!</Text>
+                {/* <Button title='Loyalty FAQ' onPress={() => navigation.navigate('LoyaltyFAQ')}/>
+                <Button title='Crumbl Cash' onPress={() => navigation.navigate('CrumblCash')} />
+                <Button title='AddPromoCode' onPress={() => navigation.navigate('AddPromoCode')}/> */}
+            </View>
+        </SafeAreaView>
   )
 }
 
