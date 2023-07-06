d78794115716d23f193e5235c4c82e04e79ab540
Mike Ramos <michaelangeloram8@gmail.com>
2023-07-05 22:51:38 -0500

    Cr8 GoogleReviewmodal

---
+++ modals/walletModals/GoogleReview.jsx
@@ -0,0 +1,12 @@
+import { View, Text, SafeAreaView } from 'react-native'
+import React from 'react'
+
+const GoogleReview = () => {
+  return (
+    <SafeAreaView className='flex-1 items-center justify-center' >
+           <Text className='text-3xl text-center'>Review us on Google</Text>
+    </SafeAreaView>
+  )
+}
+
+export default GoogleReview
