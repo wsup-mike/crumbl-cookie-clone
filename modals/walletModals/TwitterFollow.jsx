e2b3bd5084a3038b42d2be28a4a606cd0dc5540c
Mike Ramos <michaelangeloram8@gmail.com>
2023-07-05 22:52:56 -0500

    Cr8 TwitterFollow modal

---
+++ modals/walletModals/TwitterFollow.jsx
@@ -0,0 +1,12 @@
+import { View, Text, SafeAreaView } from 'react-native'
+import React from 'react'
+
+const TwitterFollow = () => {
+  return (
+    <SafeAreaView className='flex-1 items-center justify-center' >
+           <Text className='text-3xl text-center'>Follow us on Twitter</Text>
+    </SafeAreaView>
+  )
+}
+
+export default TwitterFollow
