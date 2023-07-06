8336745dad3f46edc600acfac10322b12a1876eb
Mike Ramos <michaelangeloram8@gmail.com>
2023-07-05 22:52:45 -0500

    Cr8 TikTokFollow modal

---
+++ modals/walletModals/TikTokFollow.jsx
@@ -0,0 +1,12 @@
+import { View, Text, SafeAreaView } from 'react-native'
+import React from 'react'
+
+const TikTokFollow = () => {
+  return (
+    <SafeAreaView className='flex-1 items-center justify-center' >
+           <Text className='text-3xl text-center'>Follow us on TikTok</Text>
+    </SafeAreaView>
+  )
+}
+
+export default TikTokFollow
