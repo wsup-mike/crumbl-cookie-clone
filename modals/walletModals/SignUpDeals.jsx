c05ff9eb76eb65f530bb60042637c76a67ffc74e
Mike Ramos <michaelangeloram8@gmail.com>
2023-07-05 22:24:09 -0500

    Cr8 SignUpDeals modal

---
+++ modals/walletModals/SignUpDeals.jsx
@@ -0,0 +1,12 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+const SignUpDeals = () => {
+  return (
+    <SafeAreaView className='flex-1 items-center justify-center' >
+           <Text className='text-3xl text-center'>Sign up for deals</Text>
+    </SafeAreaView>
+  )
+}
+
+export default SignUpDeals
