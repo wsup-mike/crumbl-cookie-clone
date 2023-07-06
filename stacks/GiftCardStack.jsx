aaabedf2bc8f2a895a14e855cad8a9c49b2c6535
Mike Ramos <michaelangeloram8@gmail.com>
2023-07-06 10:46:19 -0500

    presentation modal

--- stacks/GiftCardStack.jsx
+++ stacks/GiftCardStack.jsx
@@ -1,14 +1,20 @@
 import { View, Text } from 'react-native'
 import React from 'react'
 import { createStackNavigator } from '@react-navigation/stack'
+import GiftCardScreen from '../screens/GiftCardScreen'
 
 const GiftCardStack = () => {
     const Stack = createStackNavigator();
 
     return (
-        <Stack.Navigator>
-            <Stack.Screen name='' component={} options={{}} />
-            <Stack.Screen name='' component={} options={{}} />
+        <Stack.Navigator 
+            initialRouteName='GiftCardScreen'
+            screenOptions={{
+                presentation: 'modal',
+            }}
+        >
+            <Stack.Screen name='GiftCardScreen' component={GiftCardScreen} options={{}} />
+            {/* // <Stack.Screen name='' component={} options={{}} /> */}
         </Stack.Navigator>
     )
 }
