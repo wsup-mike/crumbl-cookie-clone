697cab5eca902154f0341bb3bd3bd4159bde1eaa
Mike Ramos <michaelangeloram8@gmail.com>
2023-07-05 22:51:56 -0500

    Cr8 InstagramFollow modal

---
+++ modals/walletModals/InstagramFollow.jsx
@@ -0,0 +1,12 @@
+import { View, Text, SafeAreaView } from 'react-native'
+import React from 'react'
+
+const InstagramFollow = () => {
+  return (
+    <SafeAreaView className='flex-1 items-center justify-center' >
+           <Text className='text-3xl text-center'>Follow us on Instagram</Text>
+    </SafeAreaView>
+  )
+}
+
+export default InstagramFollow
