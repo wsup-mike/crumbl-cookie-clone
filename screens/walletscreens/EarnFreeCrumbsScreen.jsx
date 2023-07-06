7fd28b336ec234f498d5228f8ae88cc8eee9317e
Mike Ramos <michaelangeloram8@gmail.com>
2023-07-05 21:27:45 -0500

    Cr8 FreeCrumbsScreen

---
+++ screens/walletscreens/EarnFreeCrumbsScreen.jsx
@@ -0,0 +1,12 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+const EarnFreeCrumbsScreen = () => {
+  return (
+    <View>
+      <Text>EarnFreeCrumbsScreen</Text>
+    </View>
+  )
+}
+
+export default EarnFreeCrumbsScreen
