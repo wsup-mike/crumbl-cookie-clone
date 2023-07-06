5bccb18a569326b93b5de41b6dc0a0b8a52d0293
Mike Ramos <michaelangeloram8@gmail.com>
2023-07-05 22:52:32 -0500

    Cr8 RateApp modal

---
+++ modals/walletModals/RateApp.jsx
@@ -0,0 +1,12 @@
+import { View, Text, SafeAreaView } from 'react-native'
+import React from 'react'
+
+const RateApp = () => {
+  return (
+    <SafeAreaView className='flex-1 items-center justify-center' >
+           <Text className='text-3xl text-center'>Rate the App</Text>
+    </SafeAreaView>
+  )
+}
+
+export default RateApp
