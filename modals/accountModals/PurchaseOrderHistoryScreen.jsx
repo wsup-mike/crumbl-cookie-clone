a30fc72f267d334dcc64c7c25038c834d83dd21b
Mike Ramos <michaelangeloram8@gmail.com>
2023-07-07 14:30:49 -0500

    Add OrderScreen button

--- modals/accountModals/PurchaseOrderHistoryScreen.jsx
+++ modals/accountModals/PurchaseOrderHistoryScreen.jsx
@@ -1,12 +1,16 @@
-import { View, Text, SafeAreaView } from 'react-native'
+import { View, Text, SafeAreaView, Button } from 'react-native'
 import React from 'react'
+import { useNavigation } from '@react-navigation/native'
 
 const PurchaseOrderHistoryScreen = () => {
+  const navigation = useNavigation();
+
   return (
     <SafeAreaView className='flex-1 items-center justify-center'>
         <View>
             <Text className='text-3xl text-center'>PurchaseOrderHistoryScreen screen</Text>
             <Text className='text-2xl text-center'>A list of all user purchases ever made</Text>
+            <Button title='OrderScreen - To place an order!' onPress={() => navigation.navigate('OrderScreen')}/>
         </View>
     </SafeAreaView>
   )
