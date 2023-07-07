a20facc0b60b03beb3858a0373c62f302ae99cee
Mike Ramos <michaelangeloram8@gmail.com>
2023-07-07 13:47:32 -0500

    Add new CookieJournal Button

--- screens/AccountScreen.jsx
+++ screens/AccountScreen.jsx
@@ -1,11 +1,15 @@
-import { View, Text, SafeAreaView } from 'react-native'
+import { View, Text, SafeAreaView, Button } from 'react-native'
 import React from 'react'
+import { useNavigation } from '@react-navigation/native'
 
 const AccountScreen = () => {
+  const navigation = useNavigation();
+
   return (
     <SafeAreaView className='flex-1 items-center justify-center bg-blue-700'>
         <View>
             <Text className='text-3xl text-center'>Account Screen: Account Information</Text>
+            <Button title='CookieJournalScreen' onPress={() => navigation.navigate('CookieJournalScreen')}/>
         </View>
     </SafeAreaView>
   )
