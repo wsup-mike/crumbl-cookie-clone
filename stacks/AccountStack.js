f027aab96f195ecfaee37401b69407dbe585e62e
Mike Ramos <michaelangeloram8@gmail.com>
2023-07-07 13:44:44 -0500

    Wire up CookieJournalScreen

--- stacks/AccountStack.js
+++ stacks/AccountStack.js
@@ -2,6 +2,7 @@ import { View, Text } from "react-native";
 import React from "react";
 import { createStackNavigator } from "@react-navigation/stack";
 import AccountScreen from "../screens/AccountScreen";
+import CookieJournalScreen from "../screens/accountscreens/CookieJournalScreen";
 
 const AccountStack = () => {
   const Stack = createStackNavigator();
@@ -14,6 +15,10 @@ const AccountStack = () => {
       }}
     >
       <Stack.Screen name="AccountScreen" component={AccountScreen} />
+      <Stack.Screen
+        name="CookieJournalScreen"
+        component={CookieJournalScreen}
+      />
     </Stack.Navigator>
   );
 };
