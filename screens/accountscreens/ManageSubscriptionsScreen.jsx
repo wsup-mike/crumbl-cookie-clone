a5e08d8e750ca1b77beac83d0e1e349dc519f9a2
Mike Ramos <michaelangeloram8@gmail.com>
2023-07-07 15:02:14 -0500

    Add SubscriptionOption button

--- screens/accountscreens/ManageSubscriptionsScreen.jsx
+++ screens/accountscreens/ManageSubscriptionsScreen.jsx
@@ -1,12 +1,19 @@
-import { View, Text, SafeAreaView } from 'react-native'
+import { View, Text, SafeAreaView, Button } from 'react-native'
 import React from 'react'
+import { useNavigation } from '@react-navigation/native'
 
 const ManageSubscriptionsScreen = () => {
+  const navigation = useNavigation();
+
   return (
     <SafeAreaView className='flex-1 items-center justify-center'>
         <View>
             <Text className='text-3xl text-center'>ManageSubscriptionsScreen screen</Text>
             <Text className='text-2xl text-center'>To manage all user current subscriptions or create new one</Text>
+            <Button 
+              title='Take me to to Subscriptions (SubscriptionOption)' 
+              onPress={() => navigation.navigate('SubscriptionOption')} 
+            />
         </View>
     </SafeAreaView>
   )
