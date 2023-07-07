265a526657b3fab597add42dfa6afe1607063d67
Mike Ramos <michaelangeloram8@gmail.com>
2023-07-07 15:31:13 -0500

    Add AddCardScreen button

--- screens/orderscreens/PaymentMethodScreen.jsx
+++ screens/orderscreens/PaymentMethodScreen.jsx
@@ -1,11 +1,16 @@
-import { View, Text, SafeAreaView } from 'react-native'
+import { View, Text, SafeAreaView, Button } from 'react-native'
 import React from 'react'
+import { useNavigation } from '@react-navigation/native'
 
 const PaymentMethodScreen = () => {
+
+  const navigation = useNavigation();
+
   return (
     <SafeAreaView className='flex-1 items-center justify-center'>
         <View>
             <Text className='text-3xl text-center'>PaymentMethodScreen: To pay</Text>
+            <Button title='AddCardScreen - Add new CC info' onPress={() => navigation.navigate('AddCardScreen')} />
         </View>
     </SafeAreaView>
   )
