70ff02f05643bb23f4c5eff45a5e4fa55bea2a73
Mike Ramos <michaelangeloram8@gmail.com>
2023-07-10 09:37:54 -0500

    Cr8 HomeStack

---
+++ stacks/HomeStack.js
@@ -0,0 +1,18 @@
+import { View, Text, SafeAreaView } from "react-native";
+import React from "react";
+import { createStackNavigator } from "@react-navigation/stack";
+import HomeScreen from "../screens/HomeScreen";
+import OrderScreen from "../screens/OrderScreen";
+
+const HomeStack = () => {
+  const Stack = createStackNavigator();
+
+  return (
+    <Stack.Navigator>
+      <Stack.Screen name="HomeScreen" component={HomeScreen} />
+      <Stack.Screen name="OrderScreen" component={OrderScreen} />
+    </Stack.Navigator>
+  );
+};
+
+export default HomeStack;
