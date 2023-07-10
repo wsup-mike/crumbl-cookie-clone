9a6ad9cd6924e406eb3845662c98da7cfdec19bb
Mike Ramos <michaelangeloram8@gmail.com>
2023-07-10 09:39:32 -0500

    Configure button to OrderScreen

--- screens/HomeScreen.jsx
+++ screens/HomeScreen.jsx
@@ -1,40 +1,17 @@
 import { View, Text, Button, SafeAreaView } from 'react-native'
 import React, { useState } from 'react'
 import { useNavigation } from '@react-navigation/native';
-import Modal from 'react-native-modal'
-import MenuModal1 from '../modals/MenuModal1';
-
 
 const HomeScreen = () => {
-  const [modalVisible, setModalVisible] = useState(false)
 
   const navigation = useNavigation();
-
-  const openModal = () => {
-    setModalVisible(true)
-  }
-
-  const closeModal = () => {
-    setModalVisible(false)
-  }
 
   return (
       <SafeAreaView className='flex-1 items-center justify-center bg-purple-400'>
         <View>
             <Text className='text-3xl text-center'>HOME SCREEN: The Mutha Frikin Crumbl Cookie Clone App</Text>
-            <Button title='Next' onPress={()=> {navigation.navigate('MenuModal1')}}/>
+            <Button title='Order Now (OrderScreen)' onPress={()=> {navigation.navigate('OrderScreen')}}/>
         </View>
-      
-        <Modal 
-          isVisible={modalVisible}
-          onBackdropPress={closeModal}
-          onSwipeComplete={() => setModalVisible(false)}
-          swipeDirection='left'
-          // animationIn='slideInUp'
-          // animationOut='slideOutDown'
-        >
-          <MenuModal1 />
-        </Modal>
       </SafeAreaView>
   )
 }
