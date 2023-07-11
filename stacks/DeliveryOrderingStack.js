4b69f298578fd41203c0464e9426e724b311b9d7
Mike Ramos <michaelangeloram8@gmail.com>
2023-07-10 20:58:39 -0500

    Next commit

--- stacks/DeliveryOrderingStack.js
+++ stacks/DeliveryOrderingStack.js
@@ -1,26 +1,14 @@
-import { View, Text } from "react-native";
 import React from "react";
 import { createStackNavigator } from "@react-navigation/stack";
 import YourAddresses from "../modals/YourAddresses";
 import CreateNewAddress from "../modals/CreateNewAddress";
-import DeliveryModalStack from "./DeliveryModalStack";
 
 const Stack = createStackNavigator();
 
 const DeliveryOrderingStack = () => {
   return (
-    <Stack.Navigator
-      initialRouteName="YourAddresses"
-      screenOptions={{
-        headerShown: false,
-        // presentation: "modal",
-      }}
-    >
-      <Stack.Screen
-        name="YourAddresses"
-        component={YourAddresses}
-        // options={{ presentation: "modal" }}
-      />
+    <Stack.Navigator initialRouteName="YourAddresses">
+      <Stack.Screen name="YourAddresses" component={YourAddresses} />
       <Stack.Screen name="CreateNewAddress" component={CreateNewAddress} />
     </Stack.Navigator>
   );
