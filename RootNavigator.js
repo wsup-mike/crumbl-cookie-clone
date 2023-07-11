a88722d2f151bdd06aa6aabe34680ac8ceda78e4
Mike Ramos <michaelangeloram8@gmail.com>
2023-07-11 10:59:25 -0500

    Only navigate to HomeScreen

--- RootNavigator.js
+++ RootNavigator.js
@@ -3,8 +3,8 @@ import { Image } from "react-native";
 
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 
-// import HomeScreen from "./screens/HomeScreen";
-import HomeStack from "./stacks/HomeStack";
+import HomeScreen from "./screens/HomeScreen";
+// import HomeStack from "./stacks/HomeStack";
 // import OrderScreen from "./screens/OrderScreen";
 import WalletScreen from "./screens/WalletScreen";
 // import GiftCardScreen from "./screens/GiftCardScreen";
