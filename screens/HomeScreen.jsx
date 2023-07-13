556532dd2f582ce03784611305954ef898ab45a4
Mike Ramos <michaelangeloram8@gmail.com>
2023-07-13 09:53:55 -0500

    Add Modal

--- screens/HomeScreen.jsx
+++ screens/HomeScreen.jsx
@@ -1,4 +1,4 @@
-import { View, Text, Button, SafeAreaView } from 'react-native'
+import { View, Text, Button, SafeAreaView, Modal } from 'react-native'
 import React from 'react'
 import { useNavigation } from '@react-navigation/native';
 
@@ -7,16 +7,21 @@ const HomeScreen = () => {
   const navigation = useNavigation();
 
   return (
-      <SafeAreaView className='flex-1 items-center justify-center bg-purple-400'>
-        <View>
-            <Text className='text-3xl text-center'>HOME SCREEN: The Mutha Frikin Crumbl Cookie Clone App</Text>
-            <Button 
-              title='Go to OrderScreen' 
-              // onPress={() => navigation.navigate('Order', { screen: 'OrderScreen'})}
-              onPress={() => navigation.navigate('Order', { screen: 'OrderScreen'})}
-            />
+    <SafeAreaView className='flex-1 items-center justify-center bg-purple-400'>
+      <Modal visible={true}>
+        <View className='flex-1 items-center justify-center bg-slate-400 pt-10'>
+          <Text className='text-3xl text-center text-white'>This is Modal Practice</Text>
         </View>
-      </SafeAreaView>
+      </Modal>
+      <View>
+          <Text className='text-3xl text-center'>HOME SCREEN: The Mutha Frikin Crumbl Cookie Clone App</Text>
+          <Button 
+            title='Go to OrderScreen' 
+            // onPress={() => navigation.navigate('Order', { screen: 'OrderScreen'})}
+            onPress={() => navigation.navigate('Order', { screen: 'OrderScreen'})}
+          />
+      </View>
+    </SafeAreaView>
   )
 }
 
