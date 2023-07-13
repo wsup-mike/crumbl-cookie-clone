458a5091565f94fb4937ccc709f3a97d6bed2438
Mike Ramos <michaelangeloram8@gmail.com>
2023-07-13 10:14:59 -0500

    Add modal toggle buttons

--- screens/HomeScreen.jsx
+++ screens/HomeScreen.jsx
@@ -12,10 +12,18 @@ const HomeScreen = () => {
       <Modal visible={modalOpen}>
         <View className='flex-1 items-center justify-center bg-slate-400 pt-10'>
           <Text className='text-3xl text-center text-white'>This is Modal Practice</Text>
+          <Button 
+            title='Toggle Modal on/off'
+            onPress={() => setModalOpen(!modalOpen)}
+          />
         </View>
       </Modal>
       <View>
           <Text className='text-3xl text-center'>HOME SCREEN: The Mutha Frikin Crumbl Cookie Clone App</Text>
+          <Button 
+            title='Toggle Modal on/off'
+            onPress={() => setModalOpen(!modalOpen)}
+          />
           <Button 
             title='Go to OrderScreen' 
             // onPress={() => navigation.navigate('Order', { screen: 'OrderScreen'})}
