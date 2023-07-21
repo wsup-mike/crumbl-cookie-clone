9d0ac6773e0fc7e70f4b6bacb8e7a641a38b0fc6
Mike Ramos <michaelangeloram8@gmail.com>
2023-07-21 10:50:36 -0500

    imoprt useRef

--- screens/GiftCardScreen.jsx
+++ screens/GiftCardScreen.jsx
@@ -1,5 +1,5 @@
 import { View, Text, SafeAreaView, Button, Animated } from 'react-native'
-import React from 'react'
+import React, { useRef } from 'react'
 import { useNavigation } from '@react-navigation/native';
 
 const GiftCardScreen = () => {
