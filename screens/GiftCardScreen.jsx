ff87294ae22cbe9b18c33326d807e1636aff1845
Mike Ramos <michaelangeloram8@gmail.com>
2023-07-21 10:50:14 -0500

    import Animiated

--- screens/GiftCardScreen.jsx
+++ screens/GiftCardScreen.jsx
@@ -1,4 +1,4 @@
-import { View, Text, SafeAreaView, Button } from 'react-native'
+import { View, Text, SafeAreaView, Button, Animated } from 'react-native'
 import React from 'react'
 import { useNavigation } from '@react-navigation/native';
 
