7a531ad22f7e474f57fa69f79ac82c49846205b5
Mike Ramos <michaelangeloram8@gmail.com>
2023-07-21 11:08:37 -0500

    Add curlies to transform array

--- screens/GiftCardScreen.jsx
+++ screens/GiftCardScreen.jsx
@@ -20,7 +20,14 @@ const GiftCardScreen = () => {
             <Text className='text-3xl text-center'>Gift Card Screen: Purchase Digital Gift Cards</Text>
             <Button title='Gift Card Design #1' onPress={() => navigation.navigate('GiftCardSelectedScreen')} />
         </View>
-        <
+        <Animated.View 
+          style={{
+            width: 100,
+            height: 100,
+            backgroundColor: 'yellow',
+            transform: [{ translateX: translation }]
+          }}
+        />
     </SafeAreaView>
   )
 }
