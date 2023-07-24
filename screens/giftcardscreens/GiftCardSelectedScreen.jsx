4efff6fb992b0ff71cfffa2aa00fe250332716b9
Mike Ramos <michaelangeloram8@gmail.com>
2023-07-23 22:10:19 -0500

    import useState

--- screens/giftcardscreens/GiftCardSelectedScreen.jsx
+++ screens/giftcardscreens/GiftCardSelectedScreen.jsx
@@ -1,5 +1,5 @@
 import { View, Text, Button, SafeAreaView, ScrollView } from 'react-native'
-import React from 'react'
+import React, { useState } from 'react'
 import { useNavigation } from '@react-navigation/native'
 
 const GiftCardSelectedScreen = () => {
