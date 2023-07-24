f3483b7ca27ace9ae05aa5f6ada61368efbbd36d
Mike Ramos <michaelangeloram8@gmail.com>
2023-07-23 22:02:49 -0500

    Remove outer View

--- screens/giftcardscreens/GiftCardSelectedScreen.jsx
+++ screens/giftcardscreens/GiftCardSelectedScreen.jsx
@@ -1,4 +1,4 @@
-import { View, Text, Button, SafeAreaView } from 'react-native'
+import { View, Text, Button, SafeAreaView, ScrollView } from 'react-native'
 import React from 'react'
 import { useNavigation } from '@react-navigation/native'
 
@@ -8,25 +8,29 @@ const GiftCardSelectedScreen = () => {
 
     return (
         <SafeAreaView className='flex-1 items-center justify-center'>
-            <View>
-                <Text className='text-3xl text-center'>GiftCardSelectedScreen screen</Text>
-                <Text className='text-2xl text-center'>Digital Gift Card Details</Text>
-                <Button title='SelectGiftCardSendDate' onPress={() => navigation.navigate('SelectGiftCardSendDate')} />
-                <Button title='SelectGiftCardSendTime' onPress={() => navigation.navigate('SelectGiftCardSendTime')} />
-                <Button title='SendGiftCardFrom' onPress={() => navigation.navigate('SendGiftCardFrom')} />
-                <Button title='AddGiftCardRecipients' onPress={() => navigation.navigate('AddGiftCardRecipients')} />
-                <Button title='AddGiftCardTextMessage' onPress={() => navigation.navigate('AddGiftCardTextMessage')} />
-                <Button title='ReviewGiftCardOrder' onPress={() => navigation.navigate('ReviewGiftCardOrder')} />
-                <Button title='GiftCardPlaceOrder' onPress={() => navigation.navigate('GiftCardPlaceOrder')} />
-                <Button title='PriceBreakdown' onPress={() => navigation.navigate('PriceBreakdown')} />
-                <Button title='PaymentMethodScreen' onPress={() => navigation.navigate('PaymentMethodScreen')} />
-                <Button title='AddCardScreen' onPress={() => navigation.navigate('AddCardScreen')} />
-                <Button title='OrderIsProcessingScreen' onPress={() => navigation.navigate('OrderIsProcessingScreen')} />
-                <Button title='OrderSuccessScreen' onPress={() => navigation.navigate('OrderSuccessScreen')} />
-                <Button title='FinalGiftCardOrderConfirmation' onPress={() => navigation.navigate('FinalGiftCardOrderConfirmation')} />
-                <Button title='ReasonForCancellation' onPress={() => navigation.navigate('ReasonForCancellation')} />
-                
-            </View>
+            <ScrollView>
+                <View>
+                    <Text className='text-3xl text-center'>GiftCardSelectedScreen screen</Text>
+                    <Text className='text-2xl text-center'>Digital Gift Card Details</Text>
+
+                        <Button title='SelectGiftCardSendDate' onPress={() => navigation.navigate('SelectGiftCardSendDate')} />
+                        <Button title='SelectGiftCardSendTime' onPress={() => navigation.navigate('SelectGiftCardSendTime')} />
+                        <Button title='SendGiftCardFrom' onPress={() => navigation.navigate('SendGiftCardFrom')} />
+                        <Button title='AddGiftCardRecipients' onPress={() => navigation.navigate('AddGiftCardRecipients')} />
+                        <Button title='AddGiftCardTextMessage' onPress={() => navigation.navigate('AddGiftCardTextMessage')} />
+                        <Button title='ReviewGiftCardOrder' onPress={() => navigation.navigate('ReviewGiftCardOrder')} />
+                        <Button title='GiftCardPlaceOrder' onPress={() => navigation.navigate('GiftCardPlaceOrder')} />
+                        <Button title='PriceBreakdown' onPress={() => navigation.navigate('PriceBreakdown')} />
+                        <Button title='PaymentMethodScreen' onPress={() => navigation.navigate('PaymentMethodScreen')} />
+                        <Button title='AddCardScreen' onPress={() => navigation.navigate('AddCardScreen')} />
+                        <Button title='OrderIsProcessingScreen' onPress={() => navigation.navigate('OrderIsProcessingScreen')} />
+                        <Button title='OrderSuccessScreen' onPress={() => navigation.navigate('OrderSuccessScreen')} />
+                        <Button title='FinalGiftCardOrderConfirmation' onPress={() => navigation.navigate('FinalGiftCardOrderConfirmation')} />
+                        <Button title='ReasonForCancellation' onPress={() => navigation.navigate('ReasonForCancellation')} />
+
+                </View>
+            </ScrollView>
+
         </SafeAreaView>
     )
 }
