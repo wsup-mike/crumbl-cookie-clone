a983a6a79e18de36d23f2d9a28e1ab65ffe78085
Mike Ramos <michaelangeloram8@gmail.com>
2023-07-25 18:12:33 -0500

    tailwindcss classname

--- screens/GesturePracticeScreen.jsx
+++ screens/GesturePracticeScreen.jsx
@@ -3,9 +3,9 @@ import React from 'react'
 
 const GesturePracticeScreen = () => {
   return (
-    <SafeAreaView classname='flex-1 items-center bg-slate-500'>
-        <View>
-            <Text classname='text-3xl text-center font-black'>GesturePracticeScreen</Text>
+    <SafeAreaView>
+        <View classname='flex-1 items-center bg-slate-500'>
+            <Text classname='text-3xl'>GesturePracticeScreen</Text>
         </View>
     </SafeAreaView>
   )
