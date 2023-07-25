9a6ef069f8b3c8882580e4d7a08dbbeb862a6983
Mike Ramos <michaelangeloram8@gmail.com>
2023-07-25 17:44:11 -0500

    Add new screen

--- stacks/HomeStack.js
+++ stacks/HomeStack.js
@@ -2,6 +2,7 @@ import React from "react";
 import { createStackNavigator } from "@react-navigation/stack";
 import HomeScreen from "../screens/HomeScreen";
 import OrderStack from "./OrderStack";
+import GesturePracticeScreen from "../screens/GesturePracticeScreen";
 
 const HomeStack = () => {
   const Stack = createStackNavigator();
@@ -12,6 +13,10 @@ const HomeStack = () => {
       screenOptions={{ headerShown: false }}
     >
       <Stack.Screen name="HomeScreen" component={HomeScreen} />
+      <Stack.Screen
+        name="GesturePracticeScreen"
+        component={GesturePracticeScreen}
+      />
       {/* <Stack.Screen name="OrderStack" component={OrderStack} /> */}
     </Stack.Navigator>
   );
