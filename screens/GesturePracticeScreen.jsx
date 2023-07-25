6a5873dd90dfac896a8884817a2e67d161f89662
Mike Ramos <michaelangeloram8@gmail.com>
2023-07-25 17:44:56 -0500

    Cr8 GesturePracticeScreen

---
+++ screens/GesturePracticeScreen.jsx
@@ -0,0 +1,12 @@
+import { View, Text, SafeAreaView } from 'react-native'
+import React from 'react'
+
+const GesturePracticeScreen = () => {
+  return (
+    <SafeAreaView>
+      <Text>GesturePracticeScreen</Text>
+    </SafeAreaView>
+  )
+}
+
+export default GesturePracticeScreen
